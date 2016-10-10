56b47805cbe74b33bf455c23ee797a3a302037b1
Ioannis Canellos <iocanel@gmail.com>
2016-10-10 12:27:41 +0300

    Align KubernetesZipkinDiscoveryProperties with latest zipkin/kubernetes-zipkin.

--- spring-cloud-kubernetes-zipkin/src/main/java/io/fabric8/spring/cloud/kubernetes/zipkin/KubernetesZipkinDiscoveryProperties.java
+++ spring-cloud-kubernetes-zipkin/src/main/java/io/fabric8/spring/cloud/kubernetes/zipkin/KubernetesZipkinDiscoveryProperties.java
@@ -23,7 +23,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class KubernetesZipkinDiscoveryProperties {
 
     private boolean enabled = true;
-    private String serviceName = "zipkin-query";
+    private String serviceName = "zipkin";
     private String serviceNamespace;
 
 
