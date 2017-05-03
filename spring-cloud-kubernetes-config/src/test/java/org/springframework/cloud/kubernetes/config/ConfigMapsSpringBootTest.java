dc05e7e264d4a69d1aaf1979b661415ed9b7f6b0
Charles Moulliard <cmoulliard@apache.org>
2017-05-03 09:43:14 +0200

    Add Java Test case covering a more real use case where a Spring Boot application use Kube configMap to reply to a REST request. Fix #71 Define restassured within the parent pom and add a property to handle its version

---
+++ spring-cloud-kubernetes-config/src/test/java/org/springframework/cloud/kubernetes/config/ConfigMapsSpringBootTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2016 to the original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.springframework.cloud.kubernetes.config;
+
+import java.util.HashMap;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.server.mock.KubernetesServer;
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.kubernetes.config.example.App;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author <a href="mailto:cmoullia@redhat.com">Charles Moulliard</a>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+                classes = App.class,
+				properties = { "spring.application.name=configmap-example",
+	           "spring.cloud.kubernetes.reload.enabled=false"})
+public class ConfigMapsSpringBootTest {
+
+	@ClassRule
+	public static KubernetesServer server = new KubernetesServer();
+
+	public static KubernetesClient mockClient;
+
+	@Autowired(required = false)
+	Config config;
+
+	private static String APPLICATION_NAME = "configmap-example";
+
+	@Value("${local.server.port}")
+	private int port;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		mockClient = server.getClient();
+
+		//Configure the kubernetes master url to point to the mock server
+		System.setProperty(Config.KUBERNETES_MASTER_SYSTEM_PROPERTY, mockClient.getConfiguration().getMasterUrl());
+		System.setProperty(Config.KUBERNETES_TRUST_CERT_SYSTEM_PROPERTY, "true");
+		System.setProperty(Config.KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY, "false");
+		System.setProperty(Config.KUBERNETES_AUTH_TRYSERVICEACCOUNT_SYSTEM_PROPERTY, "false");
+		System.setProperty(Config.KUBERNETES_NAMESPACE_SYSTEM_PROPERTY, "test");
+
+		HashMap<String,String> data = new HashMap<>();
+		data.put("bean.message","Hello ConfigMap, %s!");
+		server.expect().withPath("/api/v1/namespaces/test/configmaps/" + APPLICATION_NAME).andReturn(200, new ConfigMapBuilder()
+			.withNewMetadata().withName(APPLICATION_NAME).endMetadata()
+			.addToData(data)
+			.build())
+			.always();
+	}
+
+
+	@Before
+	public void setUp() {
+		RestAssured.baseURI = String.format("http://localhost:%d/api/greeting", port);
+	}
+
+	@Test
+	public void testConfig() {
+		assertEquals(config.getMasterUrl(),mockClient.getConfiguration().getMasterUrl());
+		assertEquals(config.getNamespace(),mockClient.getNamespace());
+	}
+
+	@Test
+	public void testGreetingEndpoint() {
+		when().get()
+			.then()
+			.statusCode(200)
+			.body("content", is("Hello ConfigMap, World!"));
+	}
+
+	@Test
+	public void testConfigMap() {
+		ConfigMap configmap = mockClient.configMaps().inNamespace("test").withName(APPLICATION_NAME).get();
+		HashMap<String,String> keys = (HashMap<String, String>) configmap.getData();
+		assertEquals(keys.get("bean.message"),"Hello ConfigMap, %s!");
+	}
+
+}
---
+++ spring-cloud-kubernetes-config/src/test/java/org/springframework/cloud/kubernetes/config/example/GreetingController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2016 to the original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.springframework.cloud.kubernetes.config.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="mailto:cmoullia@redhat.com">Charles Moulliard</a>
+ */
+@RestController
+public class GreetingController {
+
+	private final GreetingProperties properties;
+
+	@Autowired
+	public GreetingController(GreetingProperties properties) {
+		this.properties = properties;
+	}
+
+	@RequestMapping("/api/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		String message = String.format(properties.getMessage(), name);
+		return new Greeting(message);
+	}
+}
