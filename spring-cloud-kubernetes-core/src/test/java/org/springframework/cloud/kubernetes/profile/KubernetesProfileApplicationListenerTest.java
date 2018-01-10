de6cae73557ea10aa6ae7268a16f70d2debdc264
Gytis Trikleris <gytis@redhat.com>
2018-01-10 13:22:00 +0100

    Remove duplicate logic from addKubernetesProfile

---
+++ spring-cloud-kubernetes-core/src/test/java/org/springframework/cloud/kubernetes/profile/KubernetesProfileApplicationListenerTest.java
@@ -0,0 +1,53 @@
+package org.springframework.cloud.kubernetes.profile;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.cloud.kubernetes.PodUtils;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class KubernetesProfileApplicationListenerTest {
+
+	private static final String[] ACTIVE_PROFILES = new String[0];
+
+	@Mock
+	private ConfigurableEnvironment mockEnvironment;
+
+	@Mock
+	private PodUtils mockPodUtils;
+
+	@Mock
+	private ApplicationEnvironmentPreparedEvent mockEvent;
+
+	private KubernetesProfileApplicationListener listener;
+
+	@Before
+	public void before() {
+		when(mockEnvironment.getActiveProfiles()).thenReturn(ACTIVE_PROFILES);
+		when(mockEvent.getEnvironment()).thenReturn(mockEnvironment);
+		listener = new KubernetesProfileApplicationListener(mockPodUtils);
+	}
+
+	@Test
+	public void shouldEnableKubernetesProfile() {
+		when(mockPodUtils.isInsideKubernetes()).thenReturn(true);
+		listener.onApplicationEvent(mockEvent);
+		verify(mockEnvironment).addActiveProfile("kubernetes");
+	}
+
+	@Test
+	public void shouldNotEnableKubernetesProfile() {
+		when(mockPodUtils.isInsideKubernetes()).thenReturn(false);
+		listener.onApplicationEvent(mockEvent);
+		verify(mockEnvironment, times(0)).addActiveProfile("kubernetes");
+	}
+
+}
