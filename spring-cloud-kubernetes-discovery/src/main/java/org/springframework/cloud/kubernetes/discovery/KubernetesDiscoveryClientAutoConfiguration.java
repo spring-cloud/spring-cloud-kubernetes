261510cb1043e202fcba53d58cc8bd850cc1f36a
Spencer Gibb <spencer@gibb.us>
2018-03-16 16:03:10 -0400

    formatting, remove unused log.

--- spring-cloud-kubernetes-discovery/src/main/java/org/springframework/cloud/kubernetes/discovery/KubernetesDiscoveryClientAutoConfiguration.java
+++ spring-cloud-kubernetes-discovery/src/main/java/org/springframework/cloud/kubernetes/discovery/KubernetesDiscoveryClientAutoConfiguration.java
@@ -1,8 +1,6 @@
 package org.springframework.cloud.kubernetes.discovery;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.kubernetes.registry.KubernetesRegistration;
 import org.springframework.cloud.kubernetes.registry.KubernetesServiceRegistry;
@@ -13,13 +11,10 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class KubernetesDiscoveryClientAutoConfiguration {
 
-	private static final Log log = LogFactory.getLog(KubernetesDiscoveryClientAutoConfiguration.class);
-
 	@Bean
 	public DiscoveryClient discoveryClient(KubernetesClient client,
 										   KubernetesDiscoveryProperties properties) {
-		return new KubernetesDiscoveryClient(client,
-											 properties);
+		return new KubernetesDiscoveryClient(client, properties);
 	}
 
 	@Bean
@@ -30,8 +25,7 @@ public class KubernetesDiscoveryClientAutoConfiguration {
 	@Bean
 	public KubernetesRegistration getRegistration(KubernetesClient client,
 												  KubernetesDiscoveryProperties properties) {
-		return new KubernetesRegistration(client,
-										  properties);
+		return new KubernetesRegistration(client, properties);
 	}
 
 	@Bean
