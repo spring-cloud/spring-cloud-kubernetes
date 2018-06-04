3d1534e6fbc3a0328ab4ebc00ad66e0f19175c35
Gytis Trikleris <gytis@redhat.com>
2018-06-04 13:05:09 +0200

    KubernetesHelper

---
+++ spring-cloud-kubernetes-leader/src/main/java/org/springframework/cloud/kubernetes/leader/KubernetesHelper.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.kubernetes.leader;
+
+import java.util.Map;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+/**
+ * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
+ */
+public class KubernetesHelper {
+
+	private static final String PROVIDER_KEY = "provider";
+
+	private static final String PROVIDER = "spring-cloud-kubernetes";
+
+	private static final String KIND_KEY = "kind";
+
+	public static final String KIND = "leaders";
+
+	private final LeaderProperties leaderProperties;
+
+	private final KubernetesClient kubernetesClient;
+
+	public KubernetesHelper(LeaderProperties leaderProperties, KubernetesClient kubernetesClient) {
+		this.leaderProperties = leaderProperties;
+		this.kubernetesClient = kubernetesClient;
+	}
+
+	public boolean isPodAlive(String id) {
+		return kubernetesClient.pods()
+			.inNamespace(leaderProperties.getNamespace(kubernetesClient.getNamespace()))
+			.withLabels(leaderProperties.getLabels())
+			.list()
+			.getItems()
+			.stream()
+			.map(Pod::getMetadata)
+			.map(ObjectMeta::getName)
+			.anyMatch(name -> name.equals(id));
+	}
+
+	public ConfigMap getConfigMap() {
+		return kubernetesClient.configMaps()
+			.inNamespace(leaderProperties.getNamespace(kubernetesClient.getNamespace()))
+			.withName(leaderProperties.getConfigMapName())
+			.get();
+	}
+
+	public void createConfigMap(Map<String, String> data) {
+		ConfigMap newConfigMap = new ConfigMapBuilder().withNewMetadata()
+			.withName(leaderProperties.getConfigMapName())
+			.addToLabels(PROVIDER_KEY, PROVIDER)
+			.addToLabels(KIND_KEY, KIND)
+			.endMetadata()
+			.addToData(data)
+			.build();
+
+		kubernetesClient.configMaps()
+			.inNamespace(leaderProperties.getNamespace(kubernetesClient.getNamespace()))
+			.create(newConfigMap);
+	}
+
+	public void updateConfigMap(ConfigMap configMap, Map<String, String> newData) {
+		ConfigMap newConfigMap = new ConfigMapBuilder(configMap)
+			.addToData(newData)
+			.build();
+
+		kubernetesClient.configMaps()
+			.inNamespace(leaderProperties.getNamespace(kubernetesClient.getNamespace()))
+			.withName(leaderProperties.getConfigMapName())
+			.lockResourceVersion(configMap.getMetadata().getResourceVersion())
+			.replace(newConfigMap);
+	}
+
+}
