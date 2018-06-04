81f4be5cf08b4968c2c3fa83a89682fedb29be93
Gytis Trikleris <gytis@redhat.com>
2018-06-04 18:52:26 +0200

    LeadershipController using KubernetesHelper

--- spring-cloud-kubernetes-leader/src/main/java/org/springframework/cloud/kubernetes/leader/LeadershipController.java
+++ spring-cloud-kubernetes-leader/src/main/java/org/springframework/cloud/kubernetes/leader/LeadershipController.java
@@ -16,13 +16,15 @@
 
 package org.springframework.cloud.kubernetes.leader;
 
+import java.util.Collections;
 import java.util.Map;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
-import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.integration.leader.Candidate;
+import org.springframework.integration.leader.event.LeaderEventPublisher;
 
 /**
  * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
@@ -33,49 +35,110 @@ public class LeadershipController {
 
 	private final LeaderProperties leaderProperties;
 
-	private final KubernetesClient kubernetesClient;
+	private final KubernetesHelper kubernetesHelper;
 
-	public LeadershipController(LeaderProperties leaderProperties, KubernetesClient kubernetesClient) {
+	private final LeaderEventPublisher leaderEventPublisher;
+
+	public LeadershipController(LeaderProperties leaderProperties, KubernetesHelper kubernetesHelper,
+		LeaderEventPublisher leaderEventPublisher) {
 		this.leaderProperties = leaderProperties;
-		this.kubernetesClient = kubernetesClient;
+		this.kubernetesHelper = kubernetesHelper;
+		this.leaderEventPublisher = leaderEventPublisher;
 	}
 
 	public boolean acquire(Candidate candidate) {
+		try {
+			ConfigMap configMap = kubernetesHelper.getConfigMap();
+			if (configMap == null) {
+				createLeaderConfigMap(candidate);
+				handleOnGranted(candidate);
+				return true;
+			}
+
+			Leader leader = getLeader(candidate.getRole(), configMap);
+			if (leader == null || !leader.isValid()) {
+				updateLeaderConfigMap(candidate, configMap);
+				handleOnGranted(candidate);
+				return true;
+			}
+
+			if (candidate.getId().equals(leader.getId())) {
+				return true;
+			}
+		} catch (KubernetesClientException e) {
+			LOGGER.warn("Failed to acquire leadership with role='{}' for candidate='{}': {}", candidate.getRole(),
+				candidate.getId(), e.getMessage());
+		}
+
+		handleOnFailed(candidate);
 		return false;
 	}
 
 	public boolean revoke(Candidate candidate) {
+		// Get config map
+		// Check if candidate is a leader - if not, return false
+		// Try to revoke leadership - return true
+		// Return false
+		// In case of an exception - pass it forward
 		return false;
 	}
 
 	public Leader getLeader(String role) {
-		ConfigMap configMap = getConfigMap();
-		if (configMap == null || configMap.getData() == null) {
+		try {
+			ConfigMap configMap = kubernetesHelper.getConfigMap();
+			if (configMap == null) {
+				return null;
+			}
+
+			return getLeader(role, configMap);
+		} catch (KubernetesClientException e) {
+			LOGGER.warn("Failed to get leader for role='{}': {}", role, e.getMessage());
+			return null;
+		}
+	}
+
+	private Leader getLeader(String role, ConfigMap configMap) {
+		if (configMap.getData() == null) {
 			return null;
 		}
 
 		Map<String, String> data = configMap.getData();
-		String leaderIdKey = leaderProperties.getLeaderIdPrefix() + role;
-		String leaderId = data.get(leaderIdKey);
+		String leaderKey = leaderProperties.getLeaderIdPrefix() + role;
+		String leaderId = data.get(leaderKey);
 		if (leaderId == null) {
 			return null;
 		}
 
-		return new Leader(role, leaderId);
+		return new Leader(role, leaderId, kubernetesHelper);
 	}
 
-	private ConfigMap getConfigMap() {
-		String namespace = leaderProperties.getNamespace(kubernetesClient.getNamespace());
-		String name = leaderProperties.getConfigMapName();
+	private void createLeaderConfigMap(Candidate candidate) {
+		Map<String, String> data = getLeaderData(candidate);
+		kubernetesHelper.createConfigMap(data);
+	}
+
+	private void updateLeaderConfigMap(Candidate candidate, ConfigMap configMap) {
+		Map<String, String> data = getLeaderData(candidate);
+		kubernetesHelper.updateConfigMap(configMap, data);
+	}
+
+	private void handleOnGranted(Candidate candidate) {
+		leaderEventPublisher.publishOnGranted(this, null, candidate.getRole());
 		try {
-			return kubernetesClient.configMaps()
-				.inNamespace(namespace)
-				.withName(name)
-				.get();
-		} catch (Exception e) {
-			LOGGER.warn("Failed to get ConfigMap '{}' in the namespace '{}': {}", namespace, name, e.getMessage());
-			return null;
+			candidate.onGranted(null); // TODO context
+		} catch (InterruptedException e) {
+			LOGGER.warn(e.getMessage());
+			Thread.currentThread().interrupt();
 		}
+	}
+
+	private void handleOnFailed(Candidate candidate) {
+		leaderEventPublisher.publishOnFailedToAcquire(this, null, candidate.getRole());  // TODO context
+	}
+
+	private Map<String, String> getLeaderData(Candidate candidate) {
+		String leaderKey = leaderProperties.getLeaderIdPrefix() + candidate.getRole();
+		return Collections.singletonMap(leaderKey, candidate.getId());
 	}
 
 }
