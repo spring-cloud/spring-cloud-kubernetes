810af035c4a0e94d6528aae7b7730ba3ae10eba4
Spencer Gibb <spencer@gibb.us>
2018-08-02 17:47:07 -0400

    Allows label updates.
    fixes gh-203

--- spring-cloud-kubernetes-leader/src/main/java/org/springframework/cloud/kubernetes/leader/LeaderProperties.java
+++ spring-cloud-kubernetes-leader/src/main/java/org/springframework/cloud/kubernetes/leader/LeaderProperties.java
@@ -1,22 +1,24 @@
 /*
- * Copyright (C) 2018 to the original authors.
+ * Copyright 2013-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package org.springframework.cloud.kubernetes.leader;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -65,7 +67,7 @@ public class LeaderProperties {
 	 * Kubernetes labels common to all leadership candidates.
 	 * Default: empty
 	 */
-	private Map<String, String> labels = Collections.emptyMap();
+	private Map<String, String> labels = new HashMap<>();
 
 	/**
 	 * Leader id property prefix for the ConfigMap.
