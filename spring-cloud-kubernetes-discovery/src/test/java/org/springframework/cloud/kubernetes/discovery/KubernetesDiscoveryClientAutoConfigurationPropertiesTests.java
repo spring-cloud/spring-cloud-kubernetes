5e2859bf6be343bb747e60177fd6a339ebd671a1
Spencer Gibb <spencer@gibb.us>
2018-12-07 12:29:28 -0500

    Updates test for new state of application context

--- spring-cloud-kubernetes-discovery/src/test/java/org/springframework/cloud/kubernetes/discovery/KubernetesDiscoveryClientAutoConfigurationPropertiesTests.java
+++ spring-cloud-kubernetes-discovery/src/test/java/org/springframework/cloud/kubernetes/discovery/KubernetesDiscoveryClientAutoConfigurationPropertiesTests.java
@@ -3,20 +3,20 @@ package org.springframework.cloud.kubernetes.discovery;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import org.junit.After;
 import org.junit.Test;
+
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 /**
  * @author Ryan Dawson
  */
-public class KubernetesDiscoveryConfigurationTest {
+public class KubernetesDiscoveryClientAutoConfigurationPropertiesTests {
 
 	private ConfigurableApplicationContext context;
 
@@ -30,20 +30,23 @@ public class KubernetesDiscoveryConfigurationTest {
 	@Test
 	public void kubernetesDiscoveryDisabled() throws Exception {
 		setup("spring.cloud.kubernetes.discovery.enabled=false","spring.cloud.kubernetes.discovery.catalog-services-watch.enabled=false");
-		assertFalse(context.containsBean("discoveryClient"));
+		assertThat(context.getBeanNamesForType(KubernetesDiscoveryClient.class))
+			.isEmpty();
 	}
 
 	@Test
 	public void kubernetesDiscoveryWhenKubernetesDisabled() throws Exception {
 		setup("spring.cloud.kubernetes.enabled=false");
-		assertFalse(context.containsBean("discoveryClient"));
+		assertThat(context.getBeanNamesForType(KubernetesDiscoveryClient.class))
+			.isEmpty();
 	}
 
 
 	@Test
 	public void kubernetesDiscoveryDefaultEnabled() throws Exception {
 		setup("spring.cloud.kubernetes.enabled=true");
-		assertTrue(context.containsBean("discoveryClient"));
+		assertThat(context.getBeanNamesForType(KubernetesDiscoveryClient.class))
+			.hasSize(1);
 	}
 
 	private void setup(String... env) {
