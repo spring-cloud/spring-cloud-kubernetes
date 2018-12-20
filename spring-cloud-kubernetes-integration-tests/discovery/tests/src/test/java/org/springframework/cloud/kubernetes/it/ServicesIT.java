d20fb3e36d6a4f2af0f8189a156db2feaba80dd7
Georgios Andrianakis <geoand@gmail.com>
2018-12-20 16:24:10 +0200

    Add an integration test for the discovery client (#291)
    * Add an integration test for the discovery client
    
    Fixes: #288
    
    * Add missing licenses

---
+++ spring-cloud-kubernetes-integration-tests/discovery/tests/src/test/java/org/springframework/cloud/kubernetes/it/ServicesIT.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.springframework.cloud.kubernetes.it;
+
+import org.arquillian.cube.kubernetes.impl.requirement.RequiresKubernetes;
+import org.hamcrest.core.StringContains;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static io.restassured.RestAssured.given;
+
+@RequiresKubernetes
+@RunWith(Arquillian.class)
+public class ServicesIT {
+
+	private static final String HOST = System.getProperty("service.host");
+	private static final Integer PORT = Integer.valueOf(System.getProperty("service.port"));
+
+	@Test
+	public void testServicesEndpoint() {
+		given()
+			.baseUri(String.format("http://%s:%d", HOST, PORT))
+			.get("services")
+			.then()
+			.statusCode(200)
+			.body(new StringContains("service-a") {
+                @Override
+                protected boolean evalSubstringOf(String s) {
+                    return s.contains("service-a") && s.contains("service-b");
+                }
+            });
+	}
+
+}
