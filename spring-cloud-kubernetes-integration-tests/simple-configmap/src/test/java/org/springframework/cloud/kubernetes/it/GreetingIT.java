c452b02de286f0f369bdc2a79969f720eb35e802
Georgios Andrianakis <geoand@gmail.com>
2019-08-26 18:16:28 +0300

    Make security of Kubernetes Nodeport svc for integr tests configurable (#455)

--- spring-cloud-kubernetes-integration-tests/simple-configmap/src/test/java/org/springframework/cloud/kubernetes/it/GreetingIT.java
+++ spring-cloud-kubernetes-integration-tests/simple-configmap/src/test/java/org/springframework/cloud/kubernetes/it/GreetingIT.java
@@ -37,10 +37,13 @@ public class GreetingIT {
 	private static final Integer PORT = Integer
 			.valueOf(System.getProperty("service.port"));
 
+	private static final String PROTOCOL = "true"
+			.equalsIgnoreCase(System.getProperty("service.secure")) ? "https" : "http";
+
 	@Test
 	public void firstTestThatTheDefaultMessageIsReturned() {
-		given().baseUri(String.format("https://%s:%d", HOST, PORT)).get("greeting").then()
-				.statusCode(200).body("message", is("This is a dummy message"));
+		given().baseUri(String.format("%s://%s:%d", PROTOCOL, HOST, PORT)).get("greeting")
+				.then().statusCode(200).body("message", is("This is a dummy message"));
 	}
 
 	@Test
@@ -48,8 +51,8 @@ public class GreetingIT {
 	public void thenApplyAConfigMapAndEnsureThatTheMessageIsUpdated() {
 		waitForApplicationToReload();
 
-		given().baseUri(String.format("https://%s:%d", HOST, PORT)).get("greeting").then()
-				.statusCode(200)
+		given().baseUri(String.format("%s://%s:%d", PROTOCOL, HOST, PORT)).get("greeting")
+				.then().statusCode(200)
 				.body("message", is("Hello from Spring Cloud Kubernetes!"));
 	}
 
