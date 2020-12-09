3398a14b5a190acd152a8f31d61a5e3a27f7a34e
Ryan Baxter <rbaxter@vmware.com>
2020-12-08 19:42:40 -0500

    Rename Remaining Fabric8 Modules (#681)
    * Deprecate KubernetesAutoServiceRegistration
    
    * Document service registry in kubernetes.  Fixes #348
    
    * Renaming leader module to be specific to fabric8
    
    * Istio module rename
    
    * Renaming loadbalancer module

---
+++ spring-cloud-kubernetes-fabric8-leader/src/main/java/org/springframework/cloud/kubernetes/fabric8/leader/LeaderProperties.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.kubernetes.fabric8.leader;
+
+import java.time.Duration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Gytis Trikleris
+ */
+@ConfigurationProperties("spring.cloud.kubernetes.leader")
+public class LeaderProperties {
+
+	private static final boolean DEFAULT_ENABLED = true;
+
+	private static final String DEFAULT_LEADER_ID_PREFIX = "leader.id.";
+
+	private static final boolean DEFAULT_AUTO_STARTUP = true;
+
+	private static final String DEFAULT_CONFIG_MAP_NAME = "leaders";
+
+	private static final Duration DEFAULT_UPDATE_PERIOD = Duration.ofMillis(60000);
+
+	private static final boolean DEFAULT_PUBLISH_FAILED_EVENTS = false;
+
+	/**
+	 * Should leader election be enabled. Default: true
+	 */
+	private boolean enabled = DEFAULT_ENABLED;
+
+	/**
+	 * Should leader election be started automatically on startup. Default: true
+	 */
+	private boolean autoStartup = DEFAULT_AUTO_STARTUP;
+
+	/**
+	 * Role for which leadership this candidate will compete.
+	 */
+	private String role;
+
+	/**
+	 * Kubernetes namespace where the leaders ConfigMap and candidates are located.
+	 */
+	private String namespace;
+
+	/**
+	 * Kubernetes ConfigMap where leaders information will be stored. Default: leaders
+	 */
+	private String configMapName = DEFAULT_CONFIG_MAP_NAME;
+
+	/**
+	 * Leader id property prefix for the ConfigMap. Default: leader.id.
+	 */
+	private String leaderIdPrefix = DEFAULT_LEADER_ID_PREFIX;
+
+	/**
+	 * Leadership status check period. Default: 60s
+	 */
+	private Duration updatePeriod = DEFAULT_UPDATE_PERIOD;
+
+	/**
+	 * Enable/disable publishing events in case leadership acquisition fails. Default:
+	 * false
+	 */
+	private boolean publishFailedEvents = DEFAULT_PUBLISH_FAILED_EVENTS;
+
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public boolean isAutoStartup() {
+		return this.autoStartup;
+	}
+
+	public void setAutoStartup(boolean autoStartup) {
+		this.autoStartup = autoStartup;
+	}
+
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getNamespace() {
+		return this.namespace;
+	}
+
+	public void setNamespace(String namespace) {
+		this.namespace = namespace;
+	}
+
+	public String getNamespace(String defaultValue) {
+		if (this.namespace == null || this.namespace.isEmpty()) {
+			return defaultValue;
+		}
+
+		return this.namespace;
+	}
+
+	public String getConfigMapName() {
+		return this.configMapName;
+	}
+
+	public void setConfigMapName(String configMapName) {
+		this.configMapName = configMapName;
+	}
+
+	public String getLeaderIdPrefix() {
+		return this.leaderIdPrefix;
+	}
+
+	public void setLeaderIdPrefix(String leaderIdPrefix) {
+		this.leaderIdPrefix = leaderIdPrefix;
+	}
+
+	public Duration getUpdatePeriod() {
+		return this.updatePeriod;
+	}
+
+	public void setUpdatePeriod(Duration updatePeriod) {
+		this.updatePeriod = updatePeriod;
+	}
+
+	public boolean isPublishFailedEvents() {
+		return this.publishFailedEvents;
+	}
+
+	public void setPublishFailedEvents(boolean publishFailedEvents) {
+		this.publishFailedEvents = publishFailedEvents;
+	}
+
+}
---
+++ spring-cloud-kubernetes-fabric8-leader/src/test/java/org/springframework/cloud/kubernetes/fabric8/leader/LeaderRecordWatcherTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.kubernetes.fabric8.leader;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapList;
+import io.fabric8.kubernetes.api.model.DoneableConfigMap;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.Watch;
+import io.fabric8.kubernetes.client.Watcher;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Gytis Trikleris
+ */
+@ExtendWith(MockitoExtension.class)
+public class LeaderRecordWatcherTest {
+
+	@Mock
+	private LeaderProperties mockLeaderProperties;
+
+	@Mock
+	private LeadershipController mockLeadershipController;
+
+	@Mock
+	private KubernetesClient mockKubernetesClient;
+
+	@Mock
+	private MixedOperation<ConfigMap, ConfigMapList, DoneableConfigMap, Resource<ConfigMap, DoneableConfigMap>> mockConfigMapsOperation;
+
+	@Mock
+	private NonNamespaceOperation<ConfigMap, ConfigMapList, DoneableConfigMap, Resource<ConfigMap, DoneableConfigMap>> mockInNamespaceOperation;
+
+	@Mock
+	private Resource<ConfigMap, DoneableConfigMap> mockWithNameResource;
+
+	@Mock
+	private Watch mockWatch;
+
+	@Mock
+	private ConfigMap mockConfigMap;
+
+	@Mock
+	private KubernetesClientException mockKubernetesClientException;
+
+	private LeaderRecordWatcher watcher;
+
+	@BeforeEach
+	public void before() {
+		this.watcher = new LeaderRecordWatcher(this.mockLeaderProperties, this.mockLeadershipController,
+				this.mockKubernetesClient);
+	}
+
+	@Test
+	public void shouldStartOnce() {
+		initStubs();
+		this.watcher.start();
+		this.watcher.start();
+
+		verify(this.mockWithNameResource).watch(this.watcher);
+	}
+
+	@Test
+	public void shouldStopOnce() {
+		initStubs();
+		this.watcher.start();
+		this.watcher.stop();
+		this.watcher.stop();
+
+		verify(this.mockWatch).close();
+	}
+
+	@Test
+	public void shouldHandleEvent() {
+		this.watcher.eventReceived(Watcher.Action.ADDED, this.mockConfigMap);
+		this.watcher.eventReceived(Watcher.Action.DELETED, this.mockConfigMap);
+		this.watcher.eventReceived(Watcher.Action.MODIFIED, this.mockConfigMap);
+
+		verify(this.mockLeadershipController, times(3)).update();
+	}
+
+	@Test
+	public void shouldIgnoreErrorEvent() {
+		this.watcher.eventReceived(Watcher.Action.ERROR, this.mockConfigMap);
+
+		verify(this.mockLeadershipController, times(0)).update();
+	}
+
+	@Test
+	public void shouldHandleClose() {
+		initStubs();
+		this.watcher.onClose(this.mockKubernetesClientException);
+
+		verify(this.mockWithNameResource).watch(this.watcher);
+	}
+
+	@Test
+	public void shouldIgnoreCloseWithoutCause() {
+		this.watcher.onClose(null);
+
+		verify(this.mockWithNameResource, times(0)).watch(this.watcher);
+	}
+
+	private void initStubs() {
+		given(this.mockKubernetesClient.configMaps()).willReturn(this.mockConfigMapsOperation);
+		given(this.mockConfigMapsOperation.inNamespace(null)).willReturn(this.mockInNamespaceOperation);
+		given(this.mockInNamespaceOperation.withName(null)).willReturn(this.mockWithNameResource);
+		given(this.mockWithNameResource.watch(this.watcher)).willReturn(this.mockWatch);
+	}
+
+}
