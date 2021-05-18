a14bf182a6cbd841f285be9bb725859d535d4261
erabii <eugen.rabii@gmail.com>
2021-05-18 16:58:21 -0400

    some polishing (#789)

--- spring-cloud-kubernetes-client-autoconfig/src/main/java/org/springframework/cloud/kubernetes/client/KubernetesClientHealthIndicator.java
+++ spring-cloud-kubernetes-client-autoconfig/src/main/java/org/springframework/cloud/kubernetes/client/KubernetesClientHealthIndicator.java
@@ -19,7 +19,10 @@ package org.springframework.cloud.kubernetes.client;
 import java.util.Collections;
 import java.util.Map;
 
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodSpec;
+import io.kubernetes.client.openapi.models.V1PodStatus;
 
 import org.springframework.cloud.kubernetes.commons.AbstractKubernetesHealthIndicator;
 import org.springframework.cloud.kubernetes.commons.PodUtils;
@@ -42,13 +45,20 @@ public class KubernetesClientHealthIndicator extends AbstractKubernetesHealthInd
 		if (current != null) {
 			Map<String, Object> details = CollectionUtils.newHashMap(8);
 			details.put(INSIDE, true);
-			details.put(NAMESPACE, current.getMetadata().getNamespace());
-			details.put(POD_NAME, current.getMetadata().getName());
-			details.put(LABELS, current.getMetadata().getLabels());
-			details.put(POD_IP, current.getStatus().getPodIP());
-			details.put(HOST_IP, current.getStatus().getHostIP());
-			details.put(SERVICE_ACCOUNT, current.getSpec().getServiceAccountName());
-			details.put(NODE_NAME, current.getSpec().getNodeName());
+
+			V1ObjectMeta meta = current.getMetadata();
+			details.put(NAMESPACE, meta.getNamespace());
+			details.put(POD_NAME, meta.getName());
+			details.put(LABELS, meta.getLabels());
+
+			V1PodStatus status = current.getStatus();
+			details.put(POD_IP, status.getPodIP());
+			details.put(HOST_IP, status.getHostIP());
+
+			V1PodSpec spec = current.getSpec();
+			details.put(SERVICE_ACCOUNT, spec.getServiceAccountName());
+			details.put(NODE_NAME, spec.getNodeName());
+
 			return details;
 		}
 		else {
