353cfbd81bfbaecb33ad5e729c11fa3f0159f6be
Eugene <eugen.rabii@gmail.com>
2021-08-22 22:54:22 -0400

    minor rename

--- spring-cloud-kubernetes-fabric8-config/src/test/java/org/springframework/cloud/kubernetes/fabric8/config/ConfigMapWithPrefixTests.java
+++ spring-cloud-kubernetes-fabric8-config/src/test/java/org/springframework/cloud/kubernetes/fabric8/config/ConfigMapWithPrefixTests.java
@@ -39,7 +39,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
  */
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = WithPrefixApp.class,
-		properties = { "spring.cloud.bootstrap.name=same-key-with-prefix" })
+		properties = { "spring.cloud.bootstrap.name=config-map-name-as-prefix" })
 @AutoConfigureWebTestClient
 @EnableKubernetesMockClient(crud = true, https = false)
 class ConfigMapWithPrefixTests {
