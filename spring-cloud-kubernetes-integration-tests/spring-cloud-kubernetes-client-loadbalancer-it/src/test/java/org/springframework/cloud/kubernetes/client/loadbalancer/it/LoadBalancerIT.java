5bc725a095b46478c65a254410c8ead8a8e1f614
buildmaster <builds@springframework.org>
2021-09-22 11:07:38 +0000

    Update SNAPSHOT to 2.0.4

--- spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-client-loadbalancer-it/src/test/java/org/springframework/cloud/kubernetes/client/loadbalancer/it/LoadBalancerIT.java
+++ spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-client-loadbalancer-it/src/test/java/org/springframework/cloud/kubernetes/client/loadbalancer/it/LoadBalancerIT.java
@@ -211,8 +211,7 @@ public class LoadBalancerIT {
 	}
 
 	private V1Ingress getWiremockIngress() throws Exception {
-		V1Ingress ingress = (V1Ingress) k8SUtils
-				.readYamlFromClasspath("wiremock-ingress.yaml");
+		V1Ingress ingress = (V1Ingress) k8SUtils.readYamlFromClasspath("wiremock-ingress.yaml");
 		return ingress;
 	}
 
