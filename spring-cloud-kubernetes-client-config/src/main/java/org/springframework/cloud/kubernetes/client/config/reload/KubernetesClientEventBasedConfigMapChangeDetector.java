89a77c31c68dedab9c7e8478e7b7809cf3862ddb
Ryan Baxter <rbaxter@vmware.com>
2021-11-05 15:08:48 -0400

    Added some comments about why we need this change

--- spring-cloud-kubernetes-client-config/src/main/java/org/springframework/cloud/kubernetes/client/config/reload/KubernetesClientEventBasedConfigMapChangeDetector.java
+++ spring-cloud-kubernetes-client-config/src/main/java/org/springframework/cloud/kubernetes/client/config/reload/KubernetesClientEventBasedConfigMapChangeDetector.java
@@ -76,6 +76,11 @@ public class KubernetesClientEventBasedConfigMapChangeDetector extends Configura
 		super(environment, properties, strategy);
 		this.propertySourceLocator = propertySourceLocator;
 		this.coreV1Api = coreV1Api;
+		// We need to pass an APIClient to the SharedInformerFactory because if we use the default
+		// constructor it will use the configured default APIClient but that may not contain
+		// an APIClient configured within the cluster and does not contain the necessary
+		// certificate authorities for the cluster.  This results in SSL errors.
+		// See https://github.com/spring-cloud/spring-cloud-kubernetes/issues/885
 		this.factory = new SharedInformerFactory(createApiClientForInformerClient());
 		this.kubernetesNamespaceProvider = kubernetesNamespaceProvider;
 	}
