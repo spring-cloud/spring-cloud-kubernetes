a68cabc2882602d0f50786fe038977a25472bfd8
Eugene <eugen.rabii@gmail.com>
2021-11-11 08:31:09 -0500

    revert formating

--- spring-cloud-kubernetes-client-config/src/main/java/org/springframework/cloud/kubernetes/client/config/reload/KubernetesClientEventBasedSecretsChangeDetector.java
+++ spring-cloud-kubernetes-client-config/src/main/java/org/springframework/cloud/kubernetes/client/config/reload/KubernetesClientEventBasedSecretsChangeDetector.java
@@ -74,12 +74,10 @@ public class KubernetesClientEventBasedSecretsChangeDetector extends Configurati
 			KubernetesNamespaceProvider kubernetesNamespaceProvider) {
 		super(environment, properties, strategy);
 		this.propertySourceLocator = propertySourceLocator;
-		// We need to pass an APIClient to the SharedInformerFactory because if we use the
-		// default
-		// constructor it will use the configured default APIClient but that may not
-		// contain
+		// We need to pass an APIClient to the SharedInformerFactory because if we use the default
+		// constructor it will use the configured default APIClient but that may not contain
 		// an APIClient configured within the cluster and does not contain the necessary
-		// certificate authorities for the cluster. This results in SSL errors.
+		// certificate authorities for the cluster.  This results in SSL errors.
 		// See https://github.com/spring-cloud/spring-cloud-kubernetes/issues/885
 		this.factory = new SharedInformerFactory(createApiClientForInformerClient());
 		this.coreV1Api = coreV1Api;
