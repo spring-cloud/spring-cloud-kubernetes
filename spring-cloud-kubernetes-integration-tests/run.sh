156be90966d65f43e2db10eb42edeb1b5b11c003
Ryan Baxter <rbaxter@vmware.com>
2021-12-01 10:53:29 -0500

    Fixing command

--- spring-cloud-kubernetes-integration-tests/run.sh
+++ spring-cloud-kubernetes-integration-tests/run.sh
@@ -117,7 +117,7 @@ main() {
 
 	# running tests..
 	if [[ $CIRCLECI ]]; then
-		SPLIT_TESTS=circleci tests split < ${INTEGRATION_PROJECTS}
+		SPLIT_TESTS=$(circleci tests split < ${INTEGRATION_PROJECTS})
 		echo "split tests $SPLIT_TESTS"
 		PROJECT=${INTEGRATION_PROJECTS[$CIRCLE_NODE_INDEX]}
 		echo "Running test: $PROJECT"
