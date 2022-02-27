22ddd34eb679a380d5757a0a32bc501157088adb
wind57 <eugen.rabii@gmail.com>
2022-02-27 10:12:13 +0200

    log with warn

--- spring-cloud-kubernetes-fabric8-config/src/main/java/org/springframework/cloud/kubernetes/fabric8/config/Fabric8ConfigUtils.java
+++ spring-cloud-kubernetes-fabric8-config/src/main/java/org/springframework/cloud/kubernetes/fabric8/config/Fabric8ConfigUtils.java
@@ -95,7 +95,7 @@ public final class Fabric8ConfigUtils {
 		ConfigMap configMap = client.configMaps().inNamespace(namespace).withName(name).get();
 
 		if (configMap == null) {
-			LOG.debug("config-map with name : '" + name + "' not present in namespace : '" + namespace + "'");
+			LOG.warn("config-map with name : '" + name + "' not present in namespace : '" + namespace + "'");
 			return Collections.emptyMap();
 		}
 
