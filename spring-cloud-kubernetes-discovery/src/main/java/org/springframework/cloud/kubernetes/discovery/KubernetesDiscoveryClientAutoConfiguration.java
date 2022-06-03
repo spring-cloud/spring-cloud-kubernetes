72d95ec812e2d6c7c4b5f323e8c6e26dc515ae30
Ryan Baxter <rbaxter@vmware.com>
2022-06-03 11:20:49 -0400

    Make RestTemplate bean conditional.  Fixes #1025

--- spring-cloud-kubernetes-discovery/src/main/java/org/springframework/cloud/kubernetes/discovery/KubernetesDiscoveryClientAutoConfiguration.java
+++ spring-cloud-kubernetes-discovery/src/main/java/org/springframework/cloud/kubernetes/discovery/KubernetesDiscoveryClientAutoConfiguration.java
@@ -55,6 +55,7 @@ public class KubernetesDiscoveryClientAutoConfiguration {
 
 		@Bean
 		@ConditionalOnMissingClass("org.springframework.web.reactive.function.client.WebClient")
+		@ConditionalOnMissingBean(RestTemplate.class)
 		public RestTemplate restTemplate() {
 			return new RestTemplateBuilder().build();
 		}
