83a60bebf745ca38d0d78754a9aa3ae346ecd83e
wind57 <eugen.rabii@gmail.com>
2022-07-04 11:47:09 +0300

    license

--- spring-cloud-kubernetes-fabric8-config/src/main/java/org/springframework/cloud/kubernetes/fabric8/config/reload/Fabric8ConfigReloadAutoConfiguration.java
+++ spring-cloud-kubernetes-fabric8-config/src/main/java/org/springframework/cloud/kubernetes/fabric8/config/reload/Fabric8ConfigReloadAutoConfiguration.java
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
  * Copyright 2013-2022 the original author or authors.
-=======
- * Copyright 2013-2019 the original author or authors.
->>>>>>> main
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
