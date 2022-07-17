4ef205d8df9e916c038374de27d70721499fbdc2
wind57 <eugen.rabii@gmail.com>
2022-07-17 10:10:49 +0300

    more jobs + sort

--- spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-fabric8-client-simple-core/src/test/java/org/springframework/cloud/kubernetes/fabric8/core/SimpleCoreIT.java
+++ spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-fabric8-client-simple-core/src/test/java/org/springframework/cloud/kubernetes/fabric8/core/SimpleCoreIT.java
@@ -1,4 +1,4 @@
-/*
+package org.springframework.cloud.kubernetes.fabric8.core;/*
  * Copyright 2013-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
