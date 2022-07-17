a199a855f38346291dba2a180ef6b9b316e6a6d0
wind57 <eugen.rabii@gmail.com>
2022-07-17 10:11:41 +0300

    ups

--- spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-fabric8-client-simple-core/src/test/java/org/springframework/cloud/kubernetes/fabric8/core/SimpleCoreIT.java
+++ spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-fabric8-client-simple-core/src/test/java/org/springframework/cloud/kubernetes/fabric8/core/SimpleCoreIT.java
@@ -1,4 +1,4 @@
-package org.springframework.cloud.kubernetes.fabric8.core;/*
+/*
  * Copyright 2013-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,8 @@ package org.springframework.cloud.kubernetes.fabric8.core;/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.springframework.cloud.kubernetes.fabric8.core;
 
 import java.io.InputStream;
 import java.time.Duration;
