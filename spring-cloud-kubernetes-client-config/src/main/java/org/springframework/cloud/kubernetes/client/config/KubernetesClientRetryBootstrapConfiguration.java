75e59d9d9988ac2737be3035d8c4aa6ce5ca2750
Ryan Baxter <rbaxter@vmware.com>
2022-08-16 16:56:24 -0400

    Fix bad merge

--- spring-cloud-kubernetes-client-config/src/main/java/org/springframework/cloud/kubernetes/client/config/KubernetesClientRetryBootstrapConfiguration.java
+++ spring-cloud-kubernetes-client-config/src/main/java/org/springframework/cloud/kubernetes/client/config/KubernetesClientRetryBootstrapConfiguration.java
@@ -20,6 +20,8 @@ import io.kubernetes.client.openapi.apis.CoreV1Api;
 
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform;
+import org.springframework.boot.cloud.CloudPlatform;
 import org.springframework.cloud.kubernetes.client.KubernetesClientAutoConfiguration;
 import org.springframework.cloud.kubernetes.commons.KubernetesCommonsAutoConfiguration;
 import org.springframework.cloud.kubernetes.commons.KubernetesNamespaceProvider;
@@ -41,6 +43,7 @@ import org.springframework.context.annotation.Import;
 @AutoConfigureBefore(KubernetesClientBootstrapConfiguration.class)
 @Import({ KubernetesCommonsAutoConfiguration.class, KubernetesClientAutoConfiguration.class })
 @ConditionalOnKubernetesConfigOrSecretsRetryEnabled
+@ConditionalOnCloudPlatform(CloudPlatform.KUBERNETES)
 public class KubernetesClientRetryBootstrapConfiguration {
 
 	@Bean
