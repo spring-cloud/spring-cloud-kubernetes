6bae1a014dbc3aae88c7b2ee911e5c9a04a030aa
wind57 <eugen.rabii@gmail.com>
2022-09-30 21:47:08 +0300

    dirty

--- spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/discovery/KubernetesDiscoveryProperties.java
+++ spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/discovery/KubernetesDiscoveryProperties.java
@@ -16,164 +16,53 @@
 
 package org.springframework.cloud.kubernetes.commons.discovery;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
-import org.springframework.core.style.ToStringCreator;
 
 import static org.springframework.cloud.client.discovery.DiscoveryClient.DEFAULT_ORDER;
 
+/**
+ * @param enabled if kubernetes discovery is enabled
+ * @param allNamespaces if discover is enabled for all namespaces
+ * @param waitCacheReady wait for the discovery cache (service and endpoints) to be fully
+ * loaded, otherwise aborts the application on starting
+ * @param cacheLoadingTimeoutSeconds timeout for initializing discovery cache, will abort
+ * the application if exceeded.
+ * @param includeNotReadyAddresses include as discovered if endpoint addresses is not
+ * marked with 'ready' by kubernetes
+ * @param filter SpEL expression to filter services after they have been retrieved from
+ * the Kubernetes API server.
+ * @param knownSecurePorts set of known secure ports
+ * @param serviceLabels if set, then only the services matching these labels will be
+ * fetched from the Kubernetes API server.
+ * @param primaryPortName If set then the port with a given name is used as primary when
+ * multiple ports are defined for a service.
+ */
 @ConfigurationProperties("spring.cloud.kubernetes.discovery")
-public class KubernetesDiscoveryProperties {
+public record KubernetesDiscoveryProperties(@DefaultValue("true") boolean enabled, boolean allNamespaces,
+		@DefaultValue("true") boolean waitCacheReady, @DefaultValue("60") long cacheLoadingTimeoutSeconds,
+		boolean includeNotReadyAddresses, String filter, Set<Integer> knownSecurePorts,
+		Map<String, String> serviceLabels, String primaryPortName, Metadata metadata,
+		@DefaultValue(DEFAULT_ZERO_ORDER) int order) {
 
-	/** If Kubernetes Discovery is enabled. */
-	private boolean enabled = true;
+	private static final String DEFAULT_ZERO_ORDER = "" + DEFAULT_ORDER;
 
-	/** If discovering all namespaces. */
-	private boolean allNamespaces = false;
+	private static final Set<Integer> KNOWN_SECURE_PORTS = Set.of(443, 8443);
 
-	/*
-	 * If wait for the discovery cache (service and endpoints) to be fully loaded,
-	 * otherwise aborts the application on starting.
-	 */
-	private boolean waitCacheReady = true;
-
-	/**
-	 * Timeout for initializing discovery cache, will abort the application if exceeded.
-	 **/
-	private long cacheLoadingTimeoutSeconds = 60;
-
-	/**
-	 * If endpoint addresses not marked 'ready' by the k8s api server should be
-	 * discovered.
-	 */
-	private boolean includeNotReadyAddresses = false;
-
-	/**
-	 * SpEL expression to filter services AFTER they have been retrieved from the
-	 * Kubernetes API server.
-	 */
-	private String filter;
-
-	/** Set the port numbers that are considered secure and use HTTPS. */
-	private Set<Integer> knownSecurePorts = Stream.of(443, 8443).collect(Collectors.toCollection(HashSet::new));
-
-	/**
-	 * If set, then only the services matching these labels will be fetched from the
-	 * Kubernetes API server.
-	 */
-	private Map<String, String> serviceLabels = new HashMap<>();
-
-	/**
-	 * If set then the port with a given name is used as primary when multiple ports are
-	 * defined for a service.
-	 */
-	private String primaryPortName;
-
-	private Metadata metadata = new Metadata();
-
-	private int order = DEFAULT_ORDER;
-
-	public boolean isEnabled() {
-		return this.enabled;
+	public Map<String, String> serviceLabels() {
+		return serviceLabels == null ? Map.of() : serviceLabels;
 	}
 
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
+	public Metadata metadata() {
+		return metadata == null ? new Metadata() : metadata;
 	}
 
-	public String getFilter() {
-		return this.filter;
-	}
-
-	public void setFilter(String filter) {
-		this.filter = filter;
-	}
-
-	public Set<Integer> getKnownSecurePorts() {
-		return this.knownSecurePorts;
-	}
-
-	public void setKnownSecurePorts(Set<Integer> knownSecurePorts) {
-		this.knownSecurePorts = knownSecurePorts;
-	}
-
-	public Map<String, String> getServiceLabels() {
-		return this.serviceLabels;
-	}
-
-	public void setServiceLabels(Map<String, String> serviceLabels) {
-		this.serviceLabels = serviceLabels;
-	}
-
-	public String getPrimaryPortName() {
-		return primaryPortName;
-	}
-
-	public void setPrimaryPortName(String primaryPortName) {
-		this.primaryPortName = primaryPortName;
-	}
-
-	public Metadata getMetadata() {
-		return this.metadata;
-	}
-
-	public void setMetadata(Metadata metadata) {
-		this.metadata = metadata;
-	}
-
-	public boolean isAllNamespaces() {
-		return allNamespaces;
-	}
-
-	public void setAllNamespaces(boolean allNamespaces) {
-		this.allNamespaces = allNamespaces;
-	}
-
-	public boolean isIncludeNotReadyAddresses() {
-		return includeNotReadyAddresses;
-	}
-
-	public void setIncludeNotReadyAddresses(boolean includeNotReadyAddresses) {
-		this.includeNotReadyAddresses = includeNotReadyAddresses;
-	}
-
-	public int getOrder() {
-		return this.order;
-	}
-
-	public void setOrder(int order) {
-		this.order = order;
-	}
-
-	public boolean isWaitCacheReady() {
-		return waitCacheReady;
-	}
-
-	public void setWaitCacheReady(boolean waitCacheReady) {
-		this.waitCacheReady = waitCacheReady;
-	}
-
-	public long getCacheLoadingTimeoutSeconds() {
-		return cacheLoadingTimeoutSeconds;
-	}
-
-	public void setCacheLoadingTimeoutSeconds(long cacheLoadingTimeoutSeconds) {
-		this.cacheLoadingTimeoutSeconds = cacheLoadingTimeoutSeconds;
-	}
-
-	@Override
-	public String toString() {
-		return new ToStringCreator(this).append("enabled", this.enabled).append("filter", this.filter)
-				.append("knownSecurePorts", this.knownSecurePorts).append("serviceLabels", this.serviceLabels)
-				.append("metadata", this.metadata).toString();
+	public Set<Integer> knownSecurePorts() {
+		return knownSecurePorts == null ? KNOWN_SECURE_PORTS : knownSecurePorts;
 	}
 
 	/**
