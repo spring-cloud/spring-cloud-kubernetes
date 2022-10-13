86c8a7a9fa0af45f7ef66323bf27283e04616cf0
wind57 <eugen.rabii@gmail.com>
2022-10-13 12:58:37 +0300

    test

--- spring-cloud-kubernetes-commons/src/test/java/org/springframework/cloud/kubernetes/commons/config/SecretsConfigPropertiesTests.java
+++ spring-cloud-kubernetes-commons/src/test/java/org/springframework/cloud/kubernetes/commons/config/SecretsConfigPropertiesTests.java
@@ -338,17 +338,44 @@ class SecretsConfigPropertiesTests {
 	 */
 	@Test
 	void testLabelsMultipleCases() {
+
+//		SecretsConfigProperties properties = new SecretsConfigProperties();
+//		properties.setUseNameAsPrefix(false);
+//		properties.setNamespace("spring-k8s");
+//		properties.setIncludeProfileSpecificSources(false);
+//
+//		SecretsConfigProperties.Source one = new SecretsConfigProperties.Source();
+//		one.setLabels(Map.of("first-label", "secret-one"));
+//		one.setUseNameAsPrefix(false);
+//		one.setExplicitPrefix("one");
+//
+//		SecretsConfigProperties.Source two = new SecretsConfigProperties.Source();
+//		two.setLabels(Map.of("second-label", "secret-two"));
+//		two.setUseNameAsPrefix(true);
+//		two.setExplicitPrefix("two");
+//		two.setIncludeProfileSpecificSources(true);
+//
+//		SecretsConfigProperties.Source three = new SecretsConfigProperties.Source();
+//		three.setLabels(Map.of("third-label", "secret-three"));
+//		three.setExplicitPrefix("three");
+//
+//		SecretsConfigProperties.Source four = new SecretsConfigProperties.Source();
+//		four.setLabels(Map.of("fourth-label", "secret-four"));
+//
+//		properties.setSources(Arrays.asList(one, two, three, four));
+
+
 		SecretsConfigProperties properties = new SecretsConfigProperties();
 		properties.setUseNameAsPrefix(false);
 		properties.setNamespace("spring-k8s");
 		properties.setIncludeProfileSpecificSources(false);
 
 		SecretsConfigProperties.Source one = new SecretsConfigProperties.Source(
-			"one", null, Map.of("first-label", "secret-one"), "one", false, null
+			null, null, Map.of("first-label", "secret-one"), "one", false, null
 		);
 
 		SecretsConfigProperties.Source two = new SecretsConfigProperties.Source(
-			"two", null, Map.of("second-label", "secret-two"), "two", true, true
+			null, null, Map.of("second-label", "secret-two"), "two", true, true
 		);
 
 		SecretsConfigProperties.Source three = new SecretsConfigProperties.Source(
