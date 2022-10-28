347c298f550ccff8a08b40bb93e19f24f262bf05
wind57 <eugen.rabii@gmail.com>
2022-10-28 22:14:47 +0300

    checkstyle

--- spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-discoveryserver/src/main/java/org/springframewok/cloud/kubernetes/discoveryserver/DiscoveryServerController.java
+++ spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-discoveryserver/src/main/java/org/springframewok/cloud/kubernetes/discoveryserver/DiscoveryServerController.java
@@ -17,8 +17,7 @@
 package org.springframewok.cloud.kubernetes.discoveryserver;
 
 import java.util.List;
-import java.util.Objects;
-
+g
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
