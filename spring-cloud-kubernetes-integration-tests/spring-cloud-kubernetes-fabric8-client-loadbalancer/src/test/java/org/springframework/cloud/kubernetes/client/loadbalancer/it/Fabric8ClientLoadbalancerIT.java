ae3549c7fda2f0992af763df4f88bc83c1ec8a49
wind57 <eugen.rabii@gmail.com>
2022-12-07 19:16:50 +0200

    aling with fabric8 upgrade

--- spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-fabric8-client-loadbalancer/src/test/java/org/springframework/cloud/kubernetes/client/loadbalancer/it/Fabric8ClientLoadbalancerIT.java
+++ spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-fabric8-client-loadbalancer/src/test/java/org/springframework/cloud/kubernetes/client/loadbalancer/it/Fabric8ClientLoadbalancerIT.java
@@ -158,16 +158,16 @@ public class Fabric8ClientLoadbalancerIT {
 			String currentImage = deployment.getSpec().getTemplate().getSpec().getContainers().get(0).getImage();
 			deployment.getSpec().getTemplate().getSpec().getContainers().get(0).setImage(currentImage + ":" + version);
 
-			client.apps().deployments().inNamespace(NAMESPACE).create(deployment);
+			client.apps().deployments().inNamespace(NAMESPACE).resource(deployment).create();
 			deploymentName = deployment.getMetadata().getName();
 
 			Service service = client.services().load(getService()).get();
 			serviceName = service.getMetadata().getName();
-			client.services().inNamespace(NAMESPACE).create(service);
+			client.services().inNamespace(NAMESPACE).resource(service).create();
 
 			Ingress ingress = client.network().v1().ingresses().load(getIngress()).get();
 			ingressName = ingress.getMetadata().getName();
-			client.network().v1().ingresses().inNamespace(NAMESPACE).create(ingress);
+			client.network().v1().ingresses().inNamespace(NAMESPACE).resource(ingress).create();
 
 			Fabric8Utils.waitForDeployment(client, "spring-cloud-kubernetes-fabric8-client-loadbalancer-deployment",
 					NAMESPACE, 2, 600);
@@ -189,16 +189,16 @@ public class Fabric8ClientLoadbalancerIT {
 			String currentImage = deployment.getSpec().getTemplate().getSpec().getContainers().get(0).getImage();
 			deployment.getSpec().getTemplate().getSpec().getContainers().get(0).setImage(currentImage + ":" + version);
 
-			client.apps().deployments().inNamespace(NAMESPACE).create(deployment);
+			client.apps().deployments().inNamespace(NAMESPACE).resource(deployment).create();
 			deploymentName = deployment.getMetadata().getName();
 
 			Service service = client.services().load(getService()).get();
 			serviceName = service.getMetadata().getName();
-			client.services().inNamespace(NAMESPACE).create(service);
+			client.services().inNamespace(NAMESPACE).resource(service).create();
 
 			Ingress ingress = client.network().v1().ingresses().load(getIngress()).get();
 			ingressName = ingress.getMetadata().getName();
-			client.network().v1().ingresses().inNamespace(NAMESPACE).create(ingress);
+			client.network().v1().ingresses().inNamespace(NAMESPACE).resource(ingress).create();
 
 			Fabric8Utils.waitForDeployment(client, "spring-cloud-kubernetes-fabric8-client-loadbalancer-deployment",
 					NAMESPACE, 2, 600);
@@ -218,12 +218,12 @@ public class Fabric8ClientLoadbalancerIT {
 			String[] image = K8SUtils.getImageFromDeployment(deployment).split(":");
 			Commons.pullImage(image[0], image[1], K3S);
 			Commons.loadImage(image[0], image[1], "wiremock", K3S);
-			client.apps().deployments().inNamespace(NAMESPACE).create(deployment);
+			client.apps().deployments().inNamespace(NAMESPACE).resource(deployment).create();
 			mockDeploymentName = deployment.getMetadata().getName();
 
 			Service service = client.services().load(getMockService()).get();
 			mockServiceName = service.getMetadata().getName();
-			client.services().inNamespace(NAMESPACE).create(service);
+			client.services().inNamespace(NAMESPACE).resource(service).create();
 
 			Ingress ingress = client.network().v1().ingresses().load(getMockIngress()).get();
 			mockIngressName = ingress.getMetadata().getName();
