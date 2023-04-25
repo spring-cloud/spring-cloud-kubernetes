0e7ce7b39178fb880ab3abe8c640bda448bf0ebb
erabii <eugen.rabii@gmail.com>
2023-04-26 00:08:59 +0300

    Move post construct (#1319)

--- spring-cloud-kubernetes-client-discovery/src/main/java/org/springframework/cloud/kubernetes/client/discovery/KubernetesDiscoveryClientUtils.java
+++ spring-cloud-kubernetes-client-discovery/src/main/java/org/springframework/cloud/kubernetes/client/discovery/KubernetesDiscoveryClientUtils.java
@@ -16,13 +16,19 @@
 
 package org.springframework.cloud.kubernetes.client.discovery;
 
+import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Supplier;
 
+import io.kubernetes.client.informer.SharedInformerFactory;
+import io.kubernetes.client.informer.cache.Lister;
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import io.kubernetes.client.openapi.models.V1Service;
 import io.kubernetes.client.openapi.models.V1ServiceSpec;
+import io.kubernetes.client.util.wait.Wait;
 import org.apache.commons.logging.LogFactory;
 
 import org.springframework.cloud.kubernetes.commons.discovery.KubernetesDiscoveryProperties;
@@ -99,6 +105,31 @@ final class KubernetesDiscoveryClientUtils {
 				Optional.ofNullable(service.getSpec()).map(V1ServiceSpec::getType).orElse(null));
 
 		return serviceMetadata;
+	}
+
+	static void postConstruct(List<SharedInformerFactory> sharedInformerFactories,
+			KubernetesDiscoveryProperties properties, Supplier<Boolean> informersReadyFunc,
+			List<Lister<V1Service>> serviceListers) {
+
+		sharedInformerFactories.forEach(SharedInformerFactory::startAllRegisteredInformers);
+		if (!Wait.poll(Duration.ofSeconds(1), Duration.ofSeconds(properties.cacheLoadingTimeoutSeconds()), () -> {
+			LOG.info(() -> "Waiting for the cache of informers to be fully loaded..");
+			return informersReadyFunc.get();
+		})) {
+			if (properties.waitCacheReady()) {
+				throw new IllegalStateException(
+					"Timeout waiting for informers cache to be ready, is the kubernetes service up?");
+			}
+			else {
+				LOG.warn(() -> "Timeout waiting for informers cache to be ready, " +
+					"ignoring the failure because waitForInformerCacheReady property is false");
+			}
+		}
+		else {
+			LOG.info(() -> "Cache fully loaded (total " + serviceListers.stream().mapToLong(x -> x.list().size()).sum()
+				+ " services), discovery client is now available");
+		}
+
 	}
 
 }
