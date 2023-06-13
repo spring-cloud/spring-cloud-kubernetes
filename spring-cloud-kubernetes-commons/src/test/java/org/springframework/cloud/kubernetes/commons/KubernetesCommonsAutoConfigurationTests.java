7d42d8ff1c36f1d9ab65c77126ef9fe6ee0df93b
Ryan Baxter <rbaxter@vmware.com>
2023-06-13 19:09:05 -0400

    Support looking up the config server via discoveryclient (#1354)
    Fixes #1021
    
    Co-authored-by: Ryan Baxter <524254+ryanjbaxter@users.noreply.github.com>

--- spring-cloud-kubernetes-commons/src/test/java/org/springframework/cloud/kubernetes/commons/KubernetesCommonsAutoConfigurationTests.java
+++ spring-cloud-kubernetes-commons/src/test/java/org/springframework/cloud/kubernetes/commons/KubernetesCommonsAutoConfigurationTests.java
@@ -34,7 +34,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
 		classes = KubernetesCommonsAutoConfigurationTests.App.class,
 		properties = { "spring.cloud.kubernetes.client.password=mypassword",
-				"spring.cloud.kubernetes.client.proxy-password=myproxypassword" })
+				"spring.cloud.kubernetes.client.proxy-password=myproxypassword", "spring.cloud.config.enabled=false" })
 public class KubernetesCommonsAutoConfigurationTests {
 
 	@Autowired
