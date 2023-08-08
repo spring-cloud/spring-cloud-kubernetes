11edd5ed956040bb5a611b37e3b95930fd88806d
wind57 <eugen.rabii@gmail.com>
2023-08-08 11:23:12 +0300

    fix

--- spring-cloud-kubernetes-fabric8-discovery/src/test/java/org/springframework/cloud/kubernetes/fabric8/discovery/Fabric8KubernetesDiscoveryClientUtilsTests.java
+++ spring-cloud-kubernetes-fabric8-discovery/src/test/java/org/springframework/cloud/kubernetes/fabric8/discovery/Fabric8KubernetesDiscoveryClientUtilsTests.java
@@ -27,7 +27,6 @@ import io.fabric8.kubernetes.api.model.EndpointSubset;
 import io.fabric8.kubernetes.api.model.EndpointSubsetBuilder;
 import io.fabric8.kubernetes.api.model.Endpoints;
 import io.fabric8.kubernetes.api.model.EndpointsBuilder;
-import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.api.model.ServiceBuilder;
@@ -39,13 +38,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.cloud.kubernetes.commons.KubernetesNamespaceProvider;
 import org.springframework.cloud.kubernetes.commons.discovery.KubernetesDiscoveryProperties;
 import org.springframework.mock.env.MockEnvironment;
 
-import static org.springframework.cloud.kubernetes.commons.discovery.KubernetesDiscoveryConstants.PRIMARY_PORT_NAME_LABEL_KEY;
 import static org.springframework.cloud.kubernetes.fabric8.discovery.Fabric8KubernetesDiscoveryClientUtils.services;
 
 /**
@@ -88,23 +85,6 @@ class Fabric8KubernetesDiscoveryClientUtilsTests {
 	}
 
 	/*
-	 * <pre> - properties do not have primary-port-name set - service labels do not have
-	 * primary-port-name set
-	 *
-	 * As such null is returned. </pre>
-	 */
-	@Test
-	void testPrimaryPortNameNotFound(CapturedOutput output) {
-		KubernetesDiscoveryProperties properties = KubernetesDiscoveryProperties.DEFAULT;
-		Service service = new ServiceBuilder().withMetadata(new ObjectMeta()).build();
-
-		String result = Fabric8KubernetesDiscoveryClientUtils.primaryPortName(properties, service, "abc");
-		Assertions.assertNull(result);
-		Assertions.assertTrue(output.getOut().contains(
-				"did not find a primary-port-name in neither properties nor service labels for service with ID : abc"));
-	}
-
-	/*
 	 * <pre> - all-namespaces = true - serviceA present in namespace "A" - serviceB
 	 * present in namespace "B" - no filters are applied, so both are present </pre>
 	 */
@@ -120,69 +100,6 @@ class Fabric8KubernetesDiscoveryClientUtilsTests {
 		Assertions.assertEquals(result.size(), 2);
 		Assertions.assertEquals(result.stream().map(s -> s.getMetadata().getName()).sorted().toList(),
 				List.of("serviceA", "serviceB"));
-	}
-
-	/**
-	 * <pre>
-	 *     - properties do have primary-port-name set to "https"
-	 *     - service labels do not have primary-port-name set
-	 *
-	 *     As such "https" is returned.
-	 * </pre>
-	 */
-	@Test
-	void testPrimaryPortNameFoundInProperties(CapturedOutput output) {
-		String primaryPortName = "https";
-		KubernetesDiscoveryProperties properties = new KubernetesDiscoveryProperties(true, true, Set.of(), true, 60L,
-				true, "", Set.of(), Map.of(), primaryPortName, null, 0, false, false);
-		Service service = new ServiceBuilder().withMetadata(new ObjectMeta()).build();
-
-		String result = Fabric8KubernetesDiscoveryClientUtils.primaryPortName(properties, service, "abc");
-		Assertions.assertNotNull(result);
-		Assertions.assertEquals(result, primaryPortName);
-		Assertions.assertTrue(output.getOut().contains("will use primaryPortName : https for service with ID = abc"));
-	}
-
-	/**
-	 * <pre>
-	 *     - properties do not have primary-port-name set
-	 *     - service labels do have primary-port-name set to "https"
-	 *
-	 *     As such "https" is returned.
-	 * </pre>
-	 */
-	@Test
-	void testPrimaryPortNameFoundInLabels(CapturedOutput output) {
-		Map<String, String> labels = Map.of(PRIMARY_PORT_NAME_LABEL_KEY, "https");
-		KubernetesDiscoveryProperties properties = KubernetesDiscoveryProperties.DEFAULT;
-		Service service = new ServiceBuilder().withMetadata(new ObjectMetaBuilder().withLabels(labels).build()).build();
-
-		String result = Fabric8KubernetesDiscoveryClientUtils.primaryPortName(properties, service, "abc");
-		Assertions.assertNotNull(result);
-		Assertions.assertEquals(result, "https");
-		Assertions.assertTrue(output.getOut().contains("will use primaryPortName : https for service with ID = abc"));
-	}
-
-	/**
-	 * <pre>
-	 *     - properties do have primary-port-name set to "https"
-	 *     - service labels do have primary-port-name set to "http"
-	 *
-	 *     As such "http" is returned (labels win).
-	 * </pre>
-	 */
-	@Test
-	void testPrimaryPortNameFoundInBothPropertiesAndLabels(CapturedOutput output) {
-		String primaryPortName = "https";
-		Map<String, String> labels = Map.of(PRIMARY_PORT_NAME_LABEL_KEY, "http");
-		KubernetesDiscoveryProperties properties = new KubernetesDiscoveryProperties(true, true, Set.of(), true, 60L,
-				true, "", Set.of(), Map.of(), primaryPortName, null, 0, false, false);
-		Service service = new ServiceBuilder().withMetadata(new ObjectMetaBuilder().withLabels(labels).build()).build();
-
-		String result = Fabric8KubernetesDiscoveryClientUtils.primaryPortName(properties, service, "abc");
-		Assertions.assertNotNull(result);
-		Assertions.assertEquals(result, "http");
-		Assertions.assertTrue(output.getOut().contains("will use primaryPortName : http for service with ID = abc"));
 	}
 
 	/*
