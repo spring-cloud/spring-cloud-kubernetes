24e29c5c72dadbcc17991a882a8d2e02bcfd4dfd
wind57 <eugen.rabii@gmail.com>
2023-09-16 15:24:42 +0300

    started work

--- spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-client-loadbalancer-it/src/test/java/org/springframework/cloud/kubernetes/client/loadbalancer/it/LoadBalancerIT.java
+++ spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-client-loadbalancer-it/src/test/java/org/springframework/cloud/kubernetes/client/loadbalancer/it/LoadBalancerIT.java
@@ -28,7 +28,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.testcontainers.k3s.K3sContainer;
 import reactor.netty.http.client.HttpClient;
 import reactor.util.retry.Retry;
@@ -43,10 +46,36 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import static org.springframework.cloud.kubernetes.integration.tests.commons.native_client.Util.patchWithMerge;
+
 /**
  * @author Ryan Baxter
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class LoadBalancerIT {
+
+	private static final String BODY_FOR_MERGE = """
+			{
+				"spec": {
+					"template": {
+						"spec": {
+							"containers": [{
+								"name": "spring-cloud-kubernetes-client-loadbalancer-it",
+								"env": [
+								{
+									"name": "SPRING_CLOUD_KUBERNETES_LOADBALANCER_MODE",
+									"value": "SERVICE"
+								}
+								]
+							}]
+						}
+					}
+				}
+			}
+						""";
+
+	private static final Map<String, String> POD_LABELS = Map.of("app",
+			"spring-cloud-kubernetes-client-loadbalancer-it");
 
 	private static final String SERVICE_URL = "http://localhost:80/loadbalancer-it/service";
 
@@ -65,10 +94,12 @@ class LoadBalancerIT {
 		Commons.loadSpringCloudKubernetesImage(SPRING_CLOUD_K8S_LOADBALANCER_APP_NAME, K3S);
 		util = new Util(K3S);
 		util.setUp(NAMESPACE);
+		loadbalancerIt(Phase.CREATE);
 	}
 
 	@AfterAll
 	static void afterAll() throws Exception {
+		loadbalancerIt(Phase.DELETE);
 		Commons.cleanUp(SPRING_CLOUD_K8S_LOADBALANCER_APP_NAME, K3S);
 		Commons.systemPrune();
 	}
@@ -84,17 +115,16 @@ class LoadBalancerIT {
 	}
 
 	@Test
-	void testLoadBalancerServiceMode() {
-		loadbalancerIt(false, Phase.CREATE);
+	@Order(1)
+	void testLoadBalancerPodMode() {
 		testLoadBalancer();
-		loadbalancerIt(false, Phase.DELETE);
 	}
 
 	@Test
-	void testLoadBalancerPodMode() {
-		loadbalancerIt(true, Phase.CREATE);
+	@Order(2)
+	void testLoadBalancerServiceMode() {
+		patchForServiceMode("spring-cloud-kubernetes-client-loadbalancer-it-deployment", NAMESPACE);
 		testLoadBalancer();
-		loadbalancerIt(true, Phase.DELETE);
 	}
 
 	private void testLoadBalancer() {
@@ -113,10 +143,9 @@ class LoadBalancerIT {
 
 	}
 
-	private void loadbalancerIt(boolean podBased, Phase phase) {
-		V1Deployment deployment = podBased
-				? (V1Deployment) util.yaml("spring-cloud-kubernetes-client-loadbalancer-pod-it-deployment.yaml")
-				: (V1Deployment) util.yaml("spring-cloud-kubernetes-client-loadbalancer-service-it-deployment.yaml");
+	private static void loadbalancerIt(Phase phase) {
+		V1Deployment deployment = (V1Deployment) util
+				.yaml("spring-cloud-kubernetes-client-loadbalancer-pod-it-deployment.yaml");
 		V1Service service = (V1Service) util.yaml("spring-cloud-kubernetes-client-loadbalancer-it-service.yaml");
 		V1Ingress ingress = (V1Ingress) util.yaml("spring-cloud-kubernetes-client-loadbalancer-it-ingress.yaml");
 
@@ -134,6 +163,10 @@ class LoadBalancerIT {
 
 	private RetryBackoffSpec retrySpec() {
 		return Retry.fixedDelay(15, Duration.ofSeconds(1)).filter(Objects::nonNull);
+	}
+
+	private static void patchForServiceMode(String deploymentName, String namespace) {
+		patchWithMerge(deploymentName, namespace, BODY_FOR_MERGE, POD_LABELS);
 	}
 
 }
