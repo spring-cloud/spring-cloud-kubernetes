897e0060ec14509d6a78807c0aaf4cd033086fd9
wind57 <eugen.rabii@gmail.com>
2023-11-14 17:02:31 +0200

    dirty

--- spring-cloud-kubernetes-discovery/src/test/java/org/springframework/cloud/kubernetes/discovery/ConfigServerBootstrapperTests.java
+++ spring-cloud-kubernetes-discovery/src/test/java/org/springframework/cloud/kubernetes/discovery/ConfigServerBootstrapperTests.java
@@ -68,9 +68,15 @@ class ConfigServerBootstrapperTests {
 		wireMockServer = new WireMockServer(options().dynamicPort());
 		wireMockServer.start();
 		WireMock.configureFor(wireMockServer.port());
-		String APPS_NAME = "[{\"instanceId\":\"uid2\",\"serviceId\":\"spring-cloud-kubernetes-configserver\",\"host\":\"localhost\",\"port\":"
-				+ wireMockServer.port() + ",\"uri\":\"" + wireMockServer.baseUrl()
-				+ "\",\"secure\":false,\"metadata\":{\"spring\":\"true\",\"http\":\"8080\",\"k8s\":\"true\"},\"namespace\":\"namespace1\",\"cluster\":null,\"scheme\":\"http\"}]";
+		String APPS_NAME = """
+    		[{
+    			"instanceId": "uid2",
+    			"serviceId": "spring-cloud-kubernetes-configserver",
+    			"host": "localhost",
+    			"port":" + wireMockServer.port() + ","uri":"" + wireMockServer.baseUrl()
+				+ "","secure":false,"metadata":{"spring":"true","http":"8080","k8s":"true"},"namespace":"namespace1","cluster":null,"scheme":"http"}]
+		""";
+
 		stubFor(get("/apps/spring-cloud-kubernetes-configserver").willReturn(
 				aResponse().withStatus(200).withBody(APPS_NAME).withHeader("content-type", "application/json")));
 		Environment environment = new Environment("test", "default");
