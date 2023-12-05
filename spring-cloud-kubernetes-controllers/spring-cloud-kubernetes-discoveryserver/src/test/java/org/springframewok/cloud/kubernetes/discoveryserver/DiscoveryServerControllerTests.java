9930a3484112126a30115f1b00d6147d515bb12a
wind57 <eugen.rabii@gmail.com>
2023-12-05 23:18:44 +0200

    dirty

--- spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-discoveryserver/src/test/java/org/springframewok/cloud/kubernetes/discoveryserver/DiscoveryServerControllerTests.java
+++ spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-discoveryserver/src/test/java/org/springframewok/cloud/kubernetes/discoveryserver/DiscoveryServerControllerTests.java
@@ -18,8 +18,8 @@ package org.springframewok.cloud.kubernetes.discoveryserver;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -28,6 +28,7 @@ import reactor.test.StepVerifier;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.kubernetes.client.discovery.reactive.KubernetesInformerReactiveDiscoveryClient;
+import org.springframework.cloud.kubernetes.commons.discovery.DefaultKubernetesServiceInstance;
 import org.springframework.cloud.kubernetes.commons.discovery.KubernetesServiceInstance;
 
 import static org.mockito.ArgumentMatchers.eq;
@@ -39,24 +40,26 @@ import static org.mockito.Mockito.when;
  */
 class DiscoveryServerControllerTests {
 
-	private static final KubernetesServiceInstance serviceAInstance1 = new KubernetesServiceInstance("serviceAInstance1",
-		"serviceAInstance1", "2.2.2.2", 8080, new HashMap<>(), false, "namespace1", null);
+	private static final KubernetesServiceInstance SERVICE_A_INSTANCE_1 = new DefaultKubernetesServiceInstance(
+			"serviceAInstance1", "serviceAInstance1", "2.2.2.2", 8080, Map.of(), false, "namespace1", null);
 
-	private static final KubernetesServiceInstance serviceAInstance2 = new KubernetesServiceInstance("serviceAInstance2",
-		"serviceAInstance2", "2.2.2.2", 8080, new HashMap<>(), false, "namespace1", null);
+	private static final KubernetesServiceInstance SERVICE_A_INSTANCE_2 = new DefaultKubernetesServiceInstance(
+			"serviceAInstance2", "serviceAInstance2", "2.2.2.2", 8080, Map.of(), false, "namespace1", null);
 
-	private static final KubernetesServiceInstance serviceAInstance3 = new KubernetesServiceInstance("serviceAInstance3",
-		"serviceAInstance3", "2.2.2.2", 8080, new HashMap<>(), false, "namespace2", null);
+	private static final KubernetesServiceInstance SERVICE_A_INSTANCE_3 = new DefaultKubernetesServiceInstance(
+			"serviceAInstance3", "serviceAInstance3", "2.2.2.2", 8080, Map.of(), false, "namespace2", null);
 
-	private static final KubernetesServiceInstance serviceBInstance1 = new KubernetesServiceInstance("serviceBInstance1",
-		"serviceBInstance1", "2.2.2.2", 8080, new HashMap<>(), false, "namespace1", null);
+	private static final KubernetesServiceInstance SERVICE_B_INSTANCE_1 = new DefaultKubernetesServiceInstance(
+			"serviceBInstance1", "serviceBInstance1", "2.2.2.2", 8080, Map.of(), false, "namespace1", null);
 
-	private static final KubernetesServiceInstance serviceCInstance1 = new KubernetesServiceInstance("serviceCInstance1",
-		"serviceCInstance1", "2.2.2.2", 8080, new HashMap<>(), false, "namespace2", null);
+	private static final KubernetesServiceInstance SERVICE_C_INSTANCE_1 = new DefaultKubernetesServiceInstance(
+			"serviceCInstance1", "serviceCInstance1", "2.2.2.2", 8080, Map.of(), false, "namespace2", null);
 
-	private static DiscoveryServerController.Service serviceA = new DiscoveryServerController.Service();
-	private static DiscoveryServerController.Service serviceB = new DiscoveryServerController.Service();
-	private static DiscoveryServerController.Service serviceC = new DiscoveryServerController.Service();
+	private static DiscoveryServerController.Service serviceA;
+
+	private static DiscoveryServerController.Service serviceB;
+
+	private static DiscoveryServerController.Service serviceC;
 
 	private static KubernetesInformerReactiveDiscoveryClient discoveryClient;
 
@@ -65,16 +68,16 @@ class DiscoveryServerControllerTests {
 		Flux<String> services = Flux.just("serviceA", "serviceB", "serviceC");
 
 		List<ServiceInstance> serviceAInstanceList = new ArrayList<>();
-		serviceAInstanceList.add(serviceAInstance1);
-		serviceAInstanceList.add(serviceAInstance2);
-		serviceAInstanceList.add(serviceAInstance3);
+		serviceAInstanceList.add(SERVICE_A_INSTANCE_1);
+		serviceAInstanceList.add(SERVICE_A_INSTANCE_2);
+		serviceAInstanceList.add(SERVICE_A_INSTANCE_3);
 
 		Flux<ServiceInstance> serviceAInstances = Flux.fromIterable(serviceAInstanceList);
 
-		List<ServiceInstance> serviceBInstanceList = Collections.singletonList(serviceBInstance1);
+		List<ServiceInstance> serviceBInstanceList = Collections.singletonList(SERVICE_B_INSTANCE_1);
 		Flux<ServiceInstance> serviceBInstances = Flux.fromIterable(serviceBInstanceList);
 
-		List<ServiceInstance> serviceCInstanceList = Collections.singletonList(serviceCInstance1);
+		List<ServiceInstance> serviceCInstanceList = Collections.singletonList(SERVICE_C_INSTANCE_1);
 		Flux<ServiceInstance> serviceCInstances = Flux.fromIterable(serviceCInstanceList);
 
 		discoveryClient = mock(KubernetesInformerReactiveDiscoveryClient.class);
@@ -84,17 +87,11 @@ class DiscoveryServerControllerTests {
 		when(discoveryClient.getInstances(eq("serviceC"))).thenReturn(serviceCInstances);
 		when(discoveryClient.getInstances(eq("serviceD"))).thenReturn(Flux.empty());
 
-		serviceA.setName("serviceA");
-		serviceA.setServiceInstances(serviceAInstanceList);
+		serviceA = new DiscoveryServerController.Service("serviceA", serviceAInstanceList);
+		serviceB = new DiscoveryServerController.Service("serviceB", serviceBInstanceList);
+		serviceC = new DiscoveryServerController.Service("serviceC", serviceCInstanceList);
 
-		serviceB.setName("serviceB");
-		serviceB.setServiceInstances(serviceBInstanceList);
-
-		serviceC.setName("serviceC");
-		serviceC.setServiceInstances(serviceCInstanceList);
 	}
-
-
 
 	@Test
 	void apps() {
@@ -105,16 +102,19 @@ class DiscoveryServerControllerTests {
 	@Test
 	void appInstances() {
 		DiscoveryServerController controller = new DiscoveryServerController(discoveryClient);
-		StepVerifier.create(controller.appInstances("serviceA")).expectNext(serviceAInstance1, serviceAInstance2, serviceAInstance3).verifyComplete();
-		StepVerifier.create(controller.appInstances("serviceB")).expectNext(serviceBInstance1).verifyComplete();
-		StepVerifier.create(controller.appInstances("serviceC")).expectNext(serviceCInstance1).verifyComplete();
+		StepVerifier.create(controller.appInstances("serviceA"))
+				.expectNext(SERVICE_A_INSTANCE_1, SERVICE_A_INSTANCE_2, SERVICE_A_INSTANCE_3).verifyComplete();
+		StepVerifier.create(controller.appInstances("serviceB")).expectNext(SERVICE_B_INSTANCE_1).verifyComplete();
+		StepVerifier.create(controller.appInstances("serviceC")).expectNext(SERVICE_C_INSTANCE_1).verifyComplete();
 		StepVerifier.create(controller.appInstances("serviceD")).expectNextCount(0).verifyComplete();
 	}
 
 	@Test
 	void appInstance() {
 		DiscoveryServerController controller = new DiscoveryServerController(discoveryClient);
-		StepVerifier.create(controller.appInstance("serviceA", "serviceAInstance2")).expectNext(serviceAInstance2).verifyComplete();
+		StepVerifier.create(controller.appInstance("serviceA", "serviceAInstance2")).expectNext(SERVICE_A_INSTANCE_2)
+				.verifyComplete();
 		StepVerifier.create(controller.appInstance("serviceB", "doesnotexist")).expectNextCount(0).verifyComplete();
 	}
+
 }
--- spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-discoveryserver/src/test/java/org/springframewok/cloud/kubernetes/discoveryserver/DiscoveryServerIntegrationTests.java
+++ spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-discoveryserver/src/test/java/org/springframewok/cloud/kubernetes/discoveryserver/DiscoveryServerIntegrationTests.java
@@ -18,16 +18,14 @@ package org.springframewok.cloud.kubernetes.discoveryserver;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.JSON;
+import io.kubernetes.client.openapi.models.CoreV1EndpointPort;
 import io.kubernetes.client.openapi.models.V1EndpointAddress;
-import io.kubernetes.client.openapi.models.V1EndpointPort;
 import io.kubernetes.client.openapi.models.V1EndpointSubset;
 import io.kubernetes.client.openapi.models.V1Endpoints;
 import io.kubernetes.client.openapi.models.V1EndpointsListBuilder;
@@ -41,10 +39,13 @@ import io.kubernetes.client.openapi.models.V1ServiceStatus;
 import io.kubernetes.client.util.ClientBuilder;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cloud.kubernetes.client.discovery.reactive.KubernetesInformerReactiveDiscoveryClient;
 import org.springframework.cloud.kubernetes.commons.KubernetesNamespaceProvider;
+import org.springframework.cloud.kubernetes.commons.discovery.DefaultKubernetesServiceInstance;
 import org.springframework.cloud.kubernetes.commons.discovery.KubernetesServiceInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -59,150 +60,134 @@ import static org.mockito.Mockito.when;
 /**
  * @author Ryan Baxter
  */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = DiscoveryServerIntegrationTests.TestConfig.class,
-properties = {"debug=true"})
-public class DiscoveryServerIntegrationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		classes = DiscoveryServerIntegrationTests.TestConfig.class, properties = { "debug=true" })
+class DiscoveryServerIntegrationTests {
 
-	private static final V1Service testService1 = new V1Service()
-		.metadata(new V1ObjectMeta().name("test-svc-1").namespace("namespace1"))
-		.spec(new V1ServiceSpec().loadBalancerIP("1.1.1.1")).status(new V1ServiceStatus());
-	private static final V1Endpoints testEndpoints1 = new V1Endpoints()
-		.metadata(new V1ObjectMeta().name("test-svc-1").namespace("namespace1"))
-		.addSubsetsItem(new V1EndpointSubset().addPortsItem(new V1EndpointPort().port(8080).name("http"))
-			.addAddressesItem(new V1EndpointAddress().ip("2.2.2.2").targetRef(new V1ObjectReferenceBuilder().withUid("uid1").build())));
+	private static final V1Service TEST_SERVICE_1 = new V1Service()
+			.metadata(new V1ObjectMeta().name("test-svc-1").namespace("namespace1"))
+			.spec(new V1ServiceSpec().loadBalancerIP("1.1.1.1")).status(new V1ServiceStatus());
 
-	private static final V1Service testService2 = new V1Service()
-		.metadata(new V1ObjectMeta().name("test-svc-1").namespace("namespace2"))
-		.spec(new V1ServiceSpec().loadBalancerIP("1.1.1.1")).status(new V1ServiceStatus());
+	private static final V1Endpoints TEST_ENDPOINTS_1 = new V1Endpoints()
+			.metadata(new V1ObjectMeta().name("test-svc-1").namespace("namespace1"))
+			.addSubsetsItem(new V1EndpointSubset().addPortsItem(new CoreV1EndpointPort().port(8080).name("http"))
+					.addAddressesItem(new V1EndpointAddress().ip("2.2.2.2")
+							.targetRef(new V1ObjectReferenceBuilder().withUid("uid1").build())));
 
-	private static final V1Service testService3 = new V1Service()
-		.metadata(new V1ObjectMeta().name("test-svc-3").namespace("namespace1").putLabelsItem("spring", "true")
-			.putLabelsItem("k8s", "true"))
-		.spec(new V1ServiceSpec().loadBalancerIP("1.1.1.1")).status(new V1ServiceStatus());
-	private static final V1Endpoints testEndpoints3 = new V1Endpoints()
-		.metadata(new V1ObjectMeta().name("test-svc-3").namespace("namespace1"))
-		.addSubsetsItem(new V1EndpointSubset().addPortsItem(new V1EndpointPort().port(8080).name("http"))
-			.addAddressesItem(new V1EndpointAddress().ip("2.2.2.2").targetRef(new V1ObjectReferenceBuilder().withUid("uid2").build())));
+	private static final V1Service TEST_SERVICE_2 = new V1Service()
+			.metadata(new V1ObjectMeta().name("test-svc-1").namespace("namespace2"))
+			.spec(new V1ServiceSpec().loadBalancerIP("1.1.1.1")).status(new V1ServiceStatus());
 
+	private static final V1Service TEST_SERVICE_3 = new V1Service()
+			.metadata(new V1ObjectMeta().name("test-svc-3").namespace("namespace1").putLabelsItem("spring", "true")
+					.putLabelsItem("k8s", "true"))
+			.spec(new V1ServiceSpec().loadBalancerIP("1.1.1.1")).status(new V1ServiceStatus());
 
-	private static WireMockServer wireMockServer;
+	private static final V1Endpoints TEST_ENDPOINTS_3 = new V1Endpoints()
+			.metadata(new V1ObjectMeta().name("test-svc-3").namespace("namespace1"))
+			.addSubsetsItem(new V1EndpointSubset().addPortsItem(new CoreV1EndpointPort().port(8080).name("http"))
+					.addAddressesItem(new V1EndpointAddress().ip("2.2.2.2")
+							.targetRef(new V1ObjectReferenceBuilder().withUid("uid2").build())));
 
 	@Autowired
-	WebTestClient webTestClient;
+	private WebTestClient webTestClient;
 
 	@Test
-	void apps(){
+	void apps() {
 		Map<String, String> kubernetesServiceInstance1Metadata = new HashMap<>();
-		kubernetesServiceInstance1Metadata.put(testEndpoints1.getSubsets().get(0).getPorts().get(0).getName(), testEndpoints1.getSubsets().get(0).getPorts().get(0).getPort().toString());
+		kubernetesServiceInstance1Metadata.put(TEST_ENDPOINTS_1.getSubsets().get(0).getPorts().get(0).getName(),
+				TEST_ENDPOINTS_1.getSubsets().get(0).getPorts().get(0).getPort().toString());
 
 		Map<String, String> kubernetesServiceInstance2Metadata = new HashMap<>();
-		kubernetesServiceInstance2Metadata.put(testEndpoints3.getSubsets().get(0).getPorts().get(0).getName(), testEndpoints3.getSubsets().get(0).getPorts().get(0).getPort().toString());
-		kubernetesServiceInstance2Metadata.putAll(testService3.getMetadata().getLabels());
+		kubernetesServiceInstance2Metadata.put(TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getName(),
+				TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getPort().toString());
+		kubernetesServiceInstance2Metadata.putAll(TEST_SERVICE_3.getMetadata().getLabels());
 
-		KubernetesServiceInstance kubernetesServiceInstance1 = new KubernetesServiceInstance(testEndpoints1.getSubsets().get(0).getAddresses().get(0).getTargetRef().getUid(), testService1.getMetadata().getName(), testEndpoints1.getSubsets().get(0).getAddresses().get(0).getIp(), testEndpoints1.getSubsets().get(0).getPorts().get(0).getPort(), kubernetesServiceInstance1Metadata, false, testService1.getMetadata().getNamespace(), null);
-		KubernetesServiceInstance kubernetesServiceInstance3 = new KubernetesServiceInstance(testEndpoints3.getSubsets().get(0).getAddresses().get(0).getTargetRef().getUid(), testService3.getMetadata().getName(), testEndpoints3.getSubsets().get(0).getAddresses().get(0).getIp(), testEndpoints3.getSubsets().get(0).getPorts().get(0).getPort(), kubernetesServiceInstance2Metadata, false, testService3.getMetadata().getNamespace(), null);
+		KubernetesServiceInstance kubernetesServiceInstance1 = new DefaultKubernetesServiceInstance(
+				TEST_ENDPOINTS_1.getSubsets().get(0).getAddresses().get(0).getTargetRef().getUid(),
+				TEST_SERVICE_1.getMetadata().getName(), TEST_ENDPOINTS_1.getSubsets().get(0).getAddresses().get(0).getIp(),
+				TEST_ENDPOINTS_1.getSubsets().get(0).getPorts().get(0).getPort(), kubernetesServiceInstance1Metadata,
+				false, TEST_SERVICE_1.getMetadata().getNamespace(), null);
+		KubernetesServiceInstance kubernetesServiceInstance3 = new DefaultKubernetesServiceInstance(
+				TEST_ENDPOINTS_3.getSubsets().get(0).getAddresses().get(0).getTargetRef().getUid(),
+				TEST_SERVICE_3.getMetadata().getName(), TEST_ENDPOINTS_3.getSubsets().get(0).getAddresses().get(0).getIp(),
+				TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getPort(), kubernetesServiceInstance2Metadata,
+				false, TEST_SERVICE_3.getMetadata().getNamespace(), null);
 
-		webTestClient.get().uri("/apps").exchange().expectBodyList(KubernetesService.class).hasSize(2).contains(new KubernetesService(testService1.getMetadata().getName(),
-			Collections.singletonList(kubernetesServiceInstance1)), new KubernetesService(testService3.getMetadata().getName(), Collections.singletonList(kubernetesServiceInstance3)));
+		webTestClient.get().uri("/apps").exchange().expectBodyList(DiscoveryServerController.Service.class).hasSize(2).contains(
+				new DiscoveryServerController.Service(TEST_SERVICE_1.getMetadata().getName(),
+						Collections.singletonList(kubernetesServiceInstance1)),
+				new DiscoveryServerController.Service(TEST_SERVICE_3.getMetadata().getName(),
+						Collections.singletonList(kubernetesServiceInstance3)));
 	}
 
 	@Test
 	void appsName() {
 		Map<String, String> kubernetesServiceInstance2Metadata = new HashMap<>();
-		kubernetesServiceInstance2Metadata.put(testEndpoints3.getSubsets().get(0).getPorts().get(0).getName(), testEndpoints3.getSubsets().get(0).getPorts().get(0).getPort().toString());
-		kubernetesServiceInstance2Metadata.putAll(testService3.getMetadata().getLabels());
-		KubernetesServiceInstance kubernetesServiceInstance3 = new KubernetesServiceInstance(testEndpoints3.getSubsets().get(0).getAddresses().get(0).getTargetRef().getUid(), testService3.getMetadata().getName(), testEndpoints3.getSubsets().get(0).getAddresses().get(0).getIp(), testEndpoints3.getSubsets().get(0).getPorts().get(0).getPort(), kubernetesServiceInstance2Metadata, false, testService3.getMetadata().getNamespace(), null);
-		webTestClient.get().uri("/apps/test-svc-3").exchange().expectBodyList(KubernetesServiceInstance.class).hasSize(1).contains(kubernetesServiceInstance3);
+		kubernetesServiceInstance2Metadata.put(TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getName(),
+				TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getPort().toString());
+		kubernetesServiceInstance2Metadata.putAll(TEST_SERVICE_3.getMetadata().getLabels());
+		KubernetesServiceInstance kubernetesServiceInstance3 = new DefaultKubernetesServiceInstance(
+				TEST_ENDPOINTS_3.getSubsets().get(0).getAddresses().get(0).getTargetRef().getUid(),
+				TEST_SERVICE_3.getMetadata().getName(), TEST_ENDPOINTS_3.getSubsets().get(0).getAddresses().get(0).getIp(),
+				TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getPort(), kubernetesServiceInstance2Metadata,
+				false, TEST_SERVICE_3.getMetadata().getNamespace(), null);
+		webTestClient.get().uri("/apps/test-svc-3").exchange().expectBodyList(KubernetesServiceInstance.class)
+				.hasSize(1).contains(kubernetesServiceInstance3);
 	}
 
 	@Test
 	void instance() {
 		Map<String, String> kubernetesServiceInstance2Metadata = new HashMap<>();
-		kubernetesServiceInstance2Metadata.put(testEndpoints3.getSubsets().get(0).getPorts().get(0).getName(), testEndpoints3.getSubsets().get(0).getPorts().get(0).getPort().toString());
-		kubernetesServiceInstance2Metadata.putAll(testService3.getMetadata().getLabels());
-		KubernetesServiceInstance kubernetesServiceInstance3 = new KubernetesServiceInstance(testEndpoints3.getSubsets().get(0).getAddresses().get(0).getTargetRef().getUid(), testService3.getMetadata().getName(), testEndpoints3.getSubsets().get(0).getAddresses().get(0).getIp(), testEndpoints3.getSubsets().get(0).getPorts().get(0).getPort(), kubernetesServiceInstance2Metadata, false, testService3.getMetadata().getNamespace(), null);
-		webTestClient.get().uri("/app/test-svc-3/uid2").exchange().expectBody(KubernetesServiceInstance.class).isEqualTo(kubernetesServiceInstance3);
+		kubernetesServiceInstance2Metadata.put(TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getName(),
+				TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getPort().toString());
+		kubernetesServiceInstance2Metadata.putAll(TEST_SERVICE_3.getMetadata().getLabels());
+		KubernetesServiceInstance kubernetesServiceInstance3 = new DefaultKubernetesServiceInstance(
+				TEST_ENDPOINTS_3.getSubsets().get(0).getAddresses().get(0).getTargetRef().getUid(),
+				TEST_SERVICE_3.getMetadata().getName(), TEST_ENDPOINTS_3.getSubsets().get(0).getAddresses().get(0).getIp(),
+				TEST_ENDPOINTS_3.getSubsets().get(0).getPorts().get(0).getPort(), kubernetesServiceInstance2Metadata,
+				false, TEST_SERVICE_3.getMetadata().getNamespace(), null);
+		webTestClient.get().uri("/app/test-svc-3/uid2").exchange().expectBody(KubernetesServiceInstance.class)
+				.isEqualTo(kubernetesServiceInstance3);
 	}
 
-	@SpringBootApplication
+	@TestConfiguration
 	protected static class TestConfig {
 
 		@Bean
-		public KubernetesNamespaceProvider kubernetesNamespaceProvider() {
+		KubernetesNamespaceProvider kubernetesNamespaceProvider() {
 			KubernetesNamespaceProvider provider = mock(KubernetesNamespaceProvider.class);
 			when(provider.getNamespace()).thenReturn("namespace1");
 			return provider;
 		}
 
 		@Bean
-		public ApiClient apiClient() {
-			wireMockServer = new WireMockServer(options().dynamicPort());
+		ApiClient apiClient() {
+			WireMockServer wireMockServer = new WireMockServer(options().dynamicPort());
 			wireMockServer.start();
 			WireMock.configureFor(wireMockServer.port());
 			stubFor(get("/api/v1/namespaces/namespace1/endpoints?resourceVersion=0&watch=false")
-				.willReturn(aResponse().withStatus(200).withBody(new JSON().serialize(new V1EndpointsListBuilder()
-					.withMetadata(new V1ListMetaBuilder().withNewResourceVersion("0").build()).addToItems(testEndpoints1, testEndpoints3).build()))));
+					.willReturn(aResponse().withStatus(200)
+							.withBody(new JSON().serialize(
+									new V1EndpointsListBuilder().withMetadata(new V1ListMetaBuilder().build())
+											.addToItems(TEST_ENDPOINTS_1, TEST_ENDPOINTS_3).build()))));
 			stubFor(get("/api/v1/namespaces/namespace1/services?resourceVersion=0&watch=false")
-				.willReturn(aResponse().withStatus(200).withBody(new JSON().serialize(new V1ServiceListBuilder()
-					.withMetadata(new V1ListMetaBuilder().withNewResourceVersion("0").build()).addToItems(testService1, testService2, testService3).build()))));
-			stubFor(get("/api/v1/namespaces/namespace1/endpoints?watch=true")
-				.willReturn(aResponse().withStatus(200)));
-			stubFor(get("/api/v1/namespaces/namespace1/services?watch=true")
-				.willReturn(aResponse().withStatus(200)));
-			ApiClient apiClient = new ClientBuilder().setBasePath(wireMockServer.baseUrl()).build();
-			return apiClient;
+					.willReturn(aResponse().withStatus(200)
+							.withBody(new JSON()
+									.serialize(new V1ServiceListBuilder().withMetadata(new V1ListMetaBuilder().build())
+											.addToItems(TEST_SERVICE_1, TEST_SERVICE_2, TEST_SERVICE_3).build()))));
+			stubFor(get("/api/v1/namespaces/namespace1/endpoints?watch=true").willReturn(aResponse().withStatus(200)));
+			stubFor(get("/api/v1/namespaces/namespace1/services?watch=true").willReturn(aResponse().withStatus(200)));
+			return new ClientBuilder().setBasePath(wireMockServer.baseUrl()).build();
+		}
+
+		@Bean
+		KubernetesInformerReactiveDiscoveryClient discoveryClient() {
+			KubernetesInformerReactiveDiscoveryClient discoveryClient =
+				Mockito.mock(KubernetesInformerReactiveDiscoveryClient.class);
+			return discoveryClient;
 		}
 
 	}
 
-	public static class KubernetesService {
-
-		private String name;
-
-		private List<KubernetesServiceInstance> serviceInstances;
-
-		public KubernetesService() { }
-
-		public KubernetesService(String name, List<KubernetesServiceInstance> serviceInstances) {
-			this.name = name;
-			this.serviceInstances = serviceInstances;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public List<KubernetesServiceInstance> getServiceInstances() {
-			return serviceInstances;
-		}
-
-		public void setServiceInstances(List<KubernetesServiceInstance> serviceInstances) {
-			this.serviceInstances = serviceInstances;
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (o == null || getClass() != o.getClass()) return false;
-			KubernetesService service = (KubernetesService) o;
-			return Objects.equals(getName(), service.getName()) && Objects.equals(getServiceInstances(), service.getServiceInstances());
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(getName(), getServiceInstances());
-		}
-
-		@Override
-		public String toString() {
-			return "KubernetesService{" +
-				"name='" + name + '\'' +
-				", serviceInstances=" + serviceInstances +
-				'}';
-		}
-	}
 }
