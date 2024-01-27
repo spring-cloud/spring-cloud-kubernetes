e551af8d5375142c214a969654aa2efccc6d5cf7
wind57 <eugen.rabii@gmail.com>
2024-01-27 23:21:55 +0200

    dirty

--- spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/config/PropertySourceUtils.java
+++ spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/config/PropertySourceUtils.java
@@ -113,5 +113,4 @@ public final class PropertySourceUtils {
 			throw new IllegalStateException("Duplicate key " + left);
 		};
 	}
-
 }
--- spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/config/SourceDataEntriesProcessor.java
+++ spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/config/SourceDataEntriesProcessor.java
@@ -16,9 +16,15 @@
 
 package org.springframework.cloud.kubernetes.commons.config;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -32,7 +38,6 @@ import org.springframework.core.env.MapPropertySource;
 
 import static org.springframework.cloud.kubernetes.commons.config.PropertySourceUtils.KEY_VALUE_TO_PROPERTIES;
 import static org.springframework.cloud.kubernetes.commons.config.PropertySourceUtils.PROPERTIES_TO_MAP;
-import static org.springframework.cloud.kubernetes.commons.config.PropertySourceUtils.throwingMerger;
 import static org.springframework.cloud.kubernetes.commons.config.PropertySourceUtils.yamlParserGenerator;
 
 /**
@@ -76,43 +81,42 @@ public class SourceDataEntriesProcessor extends MapPropertySource {
 	private static Map<String, Object> defaultProcessAllEntries(Map<String, String> input, Environment environment) {
 
 		// we pass empty Strings on purpose, the logic here is either the value of
-		// "spring.application.name"
-		// or literal "application".
+		// "spring.application.name" or literal "application".
 		String applicationName = ConfigUtils.getApplicationName(environment, "", "");
 		String[] activeProfiles = environment.getActiveProfiles();
 
-		Set<String> fileNames = Stream
+		// the order here is important, first has to come "application.yaml" and then "application-dev.yaml"
+		List<String> orderedFileNames = Stream
 				.concat(Stream.of(applicationName),
 						Arrays.stream(activeProfiles).map(profile -> applicationName + "-" + profile))
-				.collect(Collectors.toSet());
+				.toList();
 
-		return input.entrySet().stream().map(e -> extractProperties(e.getKey(), e.getValue(), fileNames, environment))
-				.flatMap(m -> m.entrySet().stream())
-				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, throwingMerger(), HashMap::new));
-	}
-
-	private static Map<String, Object> extractProperties(String resourceName, String content, Set<String> fileNames,
-			Environment environment) {
-
-		if (resourceName.endsWith(".yml") || resourceName.endsWith(".yaml") || resourceName.endsWith(".properties")) {
-
-			if (fileNames.contains(resourceName.split("\\.", 2)[0])) {
-				if (resourceName.endsWith(".properties")) {
-					LOG.debug("entry : " + resourceName + " will be treated as a single properties file");
-					return KEY_VALUE_TO_PROPERTIES.andThen(PROPERTIES_TO_MAP).apply(content);
+		int current = orderedFileNames.size() - 1;
+		List<WeightedEntry> weightedEntries = new ArrayList<>();
+		for (Map.Entry<String, String> entry : input.entrySet()) {
+			String key = entry.getKey();
+			if (key.endsWith(".yml") || key.endsWith(".yaml") || key.endsWith(".properties")) {
+				String withoutExtension = key.split("\\.", 2)[0];
+				int index = orderedFileNames.indexOf(withoutExtension);
+				if (index >= 0) {
+					weightedEntries.add(new WeightedEntry(entry, index));
 				}
 				else {
-					LOG.debug("entry : " + resourceName + " will be treated as a single yml/yaml file");
-					return yamlParserGenerator(environment).andThen(PROPERTIES_TO_MAP).apply(content);
+					LOG.warn("entry : " + key + " will be skipped");
 				}
 			}
 			else {
-				LOG.warn("entry : " + resourceName + " will be skipped");
-				return Collections.emptyMap();
+				weightedEntries.add(new WeightedEntry(entry, ++current));
 			}
 		}
 
-		return Collections.singletonMap(resourceName, content);
+		List<Map.Entry<String, String>> sorted = weightedEntries.stream()
+			.sorted(Comparator.comparing(WeightedEntry::weight)).map(WeightedEntry::entry)
+			.toList();
+
+	}
+
+	private record WeightedEntry(Map.Entry<String, String> entry, int weight) {
 
 	}
 
