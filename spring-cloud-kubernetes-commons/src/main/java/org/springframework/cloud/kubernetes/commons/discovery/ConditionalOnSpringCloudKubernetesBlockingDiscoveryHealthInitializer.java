758c0a2c8ca5a983563bdd67197a62c227a09661
wind57 <eugen.rabii@gmail.com>
2024-02-16 16:21:44 +0200

    piggy back fix on existing PR

--- spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/discovery/ConditionalOnSpringCloudKubernetesBlockingDiscoveryHealthInitializer.java
+++ spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/discovery/ConditionalOnSpringCloudKubernetesBlockingDiscoveryHealthInitializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2023 the original author or authors.
+ * Copyright 2013-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ import org.springframework.cloud.client.ConditionalOnDiscoveryHealthIndicatorEna
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-@ConditionalOnClass({ HealthIndicator.class })
+@ConditionalOnClass(name = "org.springframework.boot.actuate.health.HealthIndicator")
 @ConditionalOnDiscoveryHealthIndicatorEnabled
 public @interface ConditionalOnSpringCloudKubernetesBlockingDiscoveryHealthInitializer {
 
