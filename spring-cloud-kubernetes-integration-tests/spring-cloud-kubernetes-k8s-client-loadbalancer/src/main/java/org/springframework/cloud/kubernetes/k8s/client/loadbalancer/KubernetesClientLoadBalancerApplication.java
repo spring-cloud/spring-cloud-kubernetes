e1b41633aeffb8a35b652db6d52c9e48f4e4eed6
wind57 <eugen.rabii@gmail.com>
2024-03-13 14:58:07 +0200

    better way

--- spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-k8s-client-loadbalancer/src/main/java/org/springframework/cloud/kubernetes/k8s/client/loadbalancer/KubernetesClientLoadBalancerApplication.java
+++ spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-k8s-client-loadbalancer/src/main/java/org/springframework/cloud/kubernetes/k8s/client/loadbalancer/KubernetesClientLoadBalancerApplication.java
@@ -63,7 +63,7 @@ class KubernetesClientLoadBalancerApplication {
 	@GetMapping("/loadbalancer-it/supplier")
 	String supplier() {
 		ServiceInstanceListSupplier supplier = loadBalancerClientFactory.getIfAvailable()
-				.getInstance("service-wiremock", ServiceInstanceListSupplier.class);
+				.getProvider("service-wiremock", ServiceInstanceListSupplier.class).getIfAvailable();
 		if (supplier instanceof CachingServiceInstanceListSupplier cachingSupplier) {
 			return cachingSupplier.getDelegate().getClass().getSimpleName();
 		}
