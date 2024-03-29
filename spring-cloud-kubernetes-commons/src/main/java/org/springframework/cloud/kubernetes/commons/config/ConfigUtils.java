5b201f0b0199001fc23c64c3c04dace1f057a5da
erabii <eugen.rabii@gmail.com>
2024-03-29 21:37:09 +0200

    cleanup for config server (#1615)

--- spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/config/ConfigUtils.java
+++ spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/config/ConfigUtils.java
@@ -194,11 +194,11 @@ public final class ConfigUtils {
 		// processed before profile based sources. This way, we replicate that
 		// "application-dev.yaml"
 		// overrides properties from "application.yaml"
-		sourceNames.forEach(source -> {
-			StrippedSourceContainer stripped = hashByName.get(source);
+		sourceNames.forEach(sourceName -> {
+			StrippedSourceContainer stripped = hashByName.get(sourceName);
 			if (stripped != null) {
-				LOG.debug("Found source with name : '" + source + " in namespace: '" + namespace + "'");
-				foundSourceNames.add(source);
+				LOG.debug("Found source with name : '" + sourceName + " in namespace: '" + namespace + "'");
+				foundSourceNames.add(sourceName);
 				// see if data is a single yaml/properties file and if it needs decoding
 				Map<String, String> rawData = stripped.data();
 				if (decode) {
@@ -214,7 +214,8 @@ public final class ConfigUtils {
 				// Check the source to see if it contains any active profiles
 				boolean containsActiveProfile = environment.getActiveProfiles().length == 0
 						|| Arrays.stream(environment.getActiveProfiles())
-								.anyMatch(p -> source.contains("-" + p) || "default".equals(p));
+								.anyMatch(activeProfile -> sourceName.endsWith("-" + activeProfile)
+									|| "default".equals(activeProfile));
 				if (includeDefaultProfileData || containsActiveProfile
 						|| containsDataWithProfile(rawData, environment.getActiveProfiles())) {
 					data.putAll(SourceDataEntriesProcessor.processAllEntries(rawData == null ? Map.of() : rawData,
@@ -232,7 +233,8 @@ public final class ConfigUtils {
 	 */
 	private static boolean containsDataWithProfile(Map<String, String> rawData, String[] activeProfiles) {
 		return rawData.keySet().stream().anyMatch(
-				key -> Arrays.stream(activeProfiles).anyMatch(p -> key.contains("-" + p) || "default".equals(p)));
+				key -> Arrays.stream(activeProfiles).anyMatch(activeProfile -> key.contains("-" + activeProfile)
+					|| "default".equals(activeProfile)));
 	}
 
 	/**
