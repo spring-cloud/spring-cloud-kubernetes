cba3a4496fc0bc511425a8b4f79fadd7d7904af9
wind57 <eugen.rabii@gmail.com>
2024-04-04 11:16:11 +0300

    fic

--- spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/loadbalancer/KubernetesServiceInstanceMapper.java
+++ spring-cloud-kubernetes-commons/src/main/java/org/springframework/cloud/kubernetes/commons/loadbalancer/KubernetesServiceInstanceMapper.java
@@ -16,7 +16,6 @@
 
 package org.springframework.cloud.kubernetes.commons.loadbalancer;
 
-import java.util.Map;
 import java.util.StringJoiner;
 
 import org.apache.commons.logging.LogFactory;
@@ -24,8 +23,6 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.kubernetes.commons.discovery.KubernetesServiceInstance;
 import org.springframework.core.log.LogAccessor;
 import org.springframework.util.StringUtils;
-
-import static org.springframework.cloud.kubernetes.commons.config.ConfigUtils.keysWithPrefix;
 
 /**
  * @author Ryan Baxter
@@ -42,43 +39,6 @@ public interface KubernetesServiceInstanceMapper<T> {
 	static String createHost(String serviceName, String namespace, String clusterDomain) {
 		String namespaceToUse = StringUtils.hasText(namespace) ? namespace : "default";
 		return new StringJoiner(".").add(serviceName).add(namespaceToUse).add("svc").add(clusterDomain).toString();
-	}
-
-	static boolean isSecure(Map<String, String> labels, Map<String, String> annotations, String servicePortName,
-			Integer servicePort) {
-
-		if (hasTrueSecuredValue(labels)) {
-			LOG.debug(() -> "Service has a true 'secured' label");
-			return true;
-		}
-
-		if (hasTrueSecuredValue(annotations)) {
-			LOG.debug(() -> "Service has a true 'secured' annotation");
-			return true;
-		}
-
-		if (servicePortName != null && servicePortName.endsWith("https")) {
-			LOG.debug(() -> "Service port name ends with 'https'");
-			return true;
-		}
-
-		if (servicePort != null && servicePort.toString().endsWith("443")) {
-			LOG.debug(() -> "Service port ends with '443'");
-			return true;
-		}
-
-		return false;
-	}
-
-	static Map<String, String> getMapWithPrefixedKeys(Map<String, String> map, String prefix) {
-		return keysWithPrefix(map, prefix);
-	}
-
-	private static boolean hasTrueSecuredValue(Map<String, String> input) {
-		if (input != null) {
-			return "true".equals(input.get("secured"));
-		}
-		return false;
 	}
 
 }
