5c811d3ca7346950f4c6cfcdeeec1bc6ca9331e7
wind57 <eugen.rabii@gmail.com>
2024-05-16 13:17:40 +0300

    checkstyle

--- spring-cloud-kubernetes-client-discovery/src/main/java/org/springframework/cloud/kubernetes/client/discovery/KubernetesClientConfigServerBootstrapper.java
+++ spring-cloud-kubernetes-client-discovery/src/main/java/org/springframework/cloud/kubernetes/client/discovery/KubernetesClientConfigServerBootstrapper.java
@@ -62,6 +62,7 @@ class KubernetesClientConfigServerBootstrapper extends KubernetesConfigServerBoo
 
 		registry.registerIfAbsent(KubernetesClientProperties.class,
 			KubernetesConfigServerBootstrapper::createKubernetesClientProperties);
+
 		registry.registerIfAbsent(ConfigServerInstanceProvider.Function.class, context -> {
 			if (!getDiscoveryEnabled(context)) {
 				return (id) -> Collections.emptyList();
