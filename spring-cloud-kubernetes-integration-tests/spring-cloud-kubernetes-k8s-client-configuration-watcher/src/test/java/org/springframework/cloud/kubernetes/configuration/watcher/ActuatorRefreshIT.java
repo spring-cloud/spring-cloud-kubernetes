7bc17c7852a7202b3eb8d9a237dd46dfd6524f2b
wind57 <eugen.rabii@gmail.com>
2025-01-31 13:30:15 +0200

    almost
    Signed-off-by: wind57 <eugen.rabii@gmail.com>

--- spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-k8s-client-configuration-watcher/src/test/java/org/springframework/cloud/kubernetes/configuration/watcher/ActuatorRefreshIT.java
+++ spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-k8s-client-configuration-watcher/src/test/java/org/springframework/cloud/kubernetes/configuration/watcher/ActuatorRefreshIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2020 the original author or authors.
+ * Copyright 2013-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,25 +16,16 @@
 
 package org.springframework.cloud.kubernetes.configuration.watcher;
 
-import java.net.SocketTimeoutException;
-import java.time.Duration;
 import java.util.List;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.stubbing.StubMapping;
-import io.kubernetes.client.openapi.models.V1ConfigMap;
-import io.kubernetes.client.openapi.models.V1ConfigMapBuilder;
 import io.kubernetes.client.openapi.models.V1Deployment;
+import io.kubernetes.client.openapi.models.V1EnvVar;
 import io.kubernetes.client.openapi.models.V1Service;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.testcontainers.containers.Container;
 import org.testcontainers.k3s.K3sContainer;
 
 import org.springframework.cloud.kubernetes.integration.tests.commons.Commons;
@@ -42,20 +33,18 @@ import org.springframework.cloud.kubernetes.integration.tests.commons.Images;
 import org.springframework.cloud.kubernetes.integration.tests.commons.Phase;
 import org.springframework.cloud.kubernetes.integration.tests.commons.native_client.Util;
 
-import static org.awaitility.Awaitility.await;
-import static org.springframework.cloud.kubernetes.integration.tests.commons.Commons.builder;
-import static org.springframework.cloud.kubernetes.integration.tests.commons.Commons.retrySpec;
+import static org.springframework.cloud.kubernetes.configuration.watcher.TestUtil.SPRING_CLOUD_K8S_CONFIG_WATCHER_APP_NAME;
+import static org.springframework.cloud.kubernetes.configuration.watcher.TestUtil.configureWireMock;
+import static org.springframework.cloud.kubernetes.configuration.watcher.TestUtil.createConfigMap;
+import static org.springframework.cloud.kubernetes.configuration.watcher.TestUtil.deleteConfigMap;
+import static org.springframework.cloud.kubernetes.configuration.watcher.TestUtil.verifyActuatorCalled;
 
 /**
  * @author Ryan Baxter
  */
 class ActuatorRefreshIT {
 
-	private static final String WIREMOCK_HOST = "localhost";
-
 	private static final String WIREMOCK_PATH = "/";
-
-	private static final int WIREMOCK_PORT = 80;
 
 	private static final String NAMESPACE = "default";
 
@@ -64,9 +53,10 @@ class ActuatorRefreshIT {
 	private static Util util;
 
 	@BeforeAll
-	static void beforeAll() {
+	static void beforeAll() throws Exception {
 		K3S.start();
-
+		Commons.validateImage(SPRING_CLOUD_K8S_CONFIG_WATCHER_APP_NAME, K3S);
+		Commons.loadSpringCloudKubernetesImage(SPRING_CLOUD_K8S_CONFIG_WATCHER_APP_NAME, K3S);
 		Images.loadWiremock(K3S);
 
 		util = new Util(K3S);
@@ -91,138 +81,45 @@ class ActuatorRefreshIT {
 	}
 
 	/*
-	 * this test loads two services: wiremock on port 8080 and configuration-watcher
-	 * on port 8888. we deploy configuration-watcher first and configure it via a
-	 * configmap with the same name. then, we mock the call to actuator/refresh endpoint
-	 * and deploy a new configmap: "service-wiremock". This in turn will trigger a
+	 * This test loads two services: wiremock on port 8080 and configuration-watcher on
+	 * port 8888. We deploy configuration-watcher first and configure its env variables
+	 * that we need for this test. Then, we mock the call to actuator/refresh endpoint and
+	 * deploy a new configmap: "service-wiremock". Because This in turn will trigger a
 	 * refresh that we capture and assert for.
 	 */
 	@Test
 	void testActuatorRefresh() {
-		WireMock.configureFor(WIREMOCK_HOST, WIREMOCK_PORT);
-		// the above statement configures the client, but we need to make sure the cluster
-		// is ready to take a request via 'Wiremock::stubFor' (because sometimes it fails)
-		// As such, get the existing mappings and retrySpec() makes sure we retry until
-		// we get a response back.
-		WebClient client = builder().baseUrl("http://localhost:80/__admin/mappings").build();
-		client.method(HttpMethod.GET)
-			.retrieve()
-			.bodyToMono(String.class)
-			.retryWhen(retrySpec())
-			.block();
+		configureWireMock();
+		createConfigMap(util, NAMESPACE);
+		verifyActuatorCalled(1);
 
-		StubMapping stubMapping = WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/actuator/refresh"))
-				.willReturn(WireMock.aResponse().withBody("{}").withStatus(200)));
+		Commons.waitForLogStatement("creating NOOP strategy because reload is disabled", K3S,
+				SPRING_CLOUD_K8S_CONFIG_WATCHER_APP_NAME);
 
-		await().atMost(Duration.ofSeconds(60))
-			.pollInterval(Duration.ofSeconds(1))
-			.ignoreException(SocketTimeoutException.class)
-			.until(() -> stubMapping.getResponse().wasConfigured());
-
-		createConfigMap();
-
-		// Wait a bit before we verify
-		await().atMost(Duration.ofSeconds(30))
-			.until(() -> !WireMock.findAll(WireMock.postRequestedFor(WireMock.urlEqualTo("/actuator/refresh")))
-				.isEmpty());
-		WireMock.verify(WireMock.postRequestedFor(WireMock.urlEqualTo("/actuator/refresh")));
-
-		deleteConfigMap();
-
-		// the other test
-		testActuatorRefreshReloadDisabled();
-
-	}
-
-	/*
-	 * same test as above, but reload is disabled.
-	 */
-	void testActuatorRefreshReloadDisabled() {
-
-		///TestUtil.patchForDisabledReload(SPRING_CLOUD_K8S_CONFIG_WATCHER_APP_NAME, NAMESPACE, DOCKER_IMAGE);
-
-		WireMock.configureFor(WIREMOCK_HOST, WIREMOCK_PORT);
-		await().timeout(Duration.ofSeconds(60))
-			.until(() -> WireMock
-				.stubFor(WireMock.post(WireMock.urlEqualTo("/actuator/refresh"))
-					.willReturn(WireMock.aResponse().withBody("{}").withStatus(200)))
-				.getResponse()
-				.wasConfigured());
-
-		createConfigMap();
-
-		// Wait a bit before we verify
-		await().atMost(Duration.ofSeconds(30))
-			.until(() -> !WireMock.findAll(WireMock.postRequestedFor(WireMock.urlEqualTo("/actuator/refresh")))
-				.isEmpty());
-
-//		Commons.waitForLogStatement("creating NOOP strategy because reload is disabled", K3S,
-//				SPRING_CLOUD_K8S_CONFIG_WATCHER_APP_NAME);
-
-		// nothing related to 'ConfigReloadUtil' is present in logs
-		// this proves that once we disable reload everything still works
-		//Assertions.assertFalse(logs().contains("ConfigReloadUtil"));
-		WireMock.verify(WireMock.postRequestedFor(WireMock.urlEqualTo("/actuator/refresh")));
-
-		deleteConfigMap();
-
+		deleteConfigMap(util, NAMESPACE);
 	}
 
 	private static void configWatcher(Phase phase) {
-		V1ConfigMap configMap = (V1ConfigMap) util
-			.yaml("config-watcher/spring-cloud-kubernetes-configuration-watcher-configmap.yaml");
 		V1Deployment deployment = (V1Deployment) util
 			.yaml("config-watcher/spring-cloud-kubernetes-configuration-watcher-deployment.yaml");
 		V1Service service = (V1Service) util
 			.yaml("config-watcher/spring-cloud-kubernetes-configuration-watcher-service.yaml");
 
+		List<V1EnvVar> envVars = List.of(
+				new V1EnvVar().name("SPRING_CLOUD_KUBERNETES_CONFIGURATION_WATCHER_REFRESHDELAY").value("0"),
+				new V1EnvVar().name("SPRING_CLOUD_KUBERNETES_RELOAD_ENABLED").value("FALSE"),
+				new V1EnvVar().name("LOGGING_LEVEL_ORG_SPRINGFRAMEWORK_CLOUD_KUBERNETES_CONFIGURATION_WATCHER")
+					.value("DEBUG"));
+
+		deployment.getSpec().getTemplate().getSpec().getContainers().get(0).setEnv(envVars);
+
 		if (phase.equals(Phase.CREATE)) {
-			util.createAndWait(NAMESPACE, configMap, null);
 			util.createAndWait(NAMESPACE, null, deployment, service, null, true);
 		}
 		else {
-			util.deleteAndWait(NAMESPACE, configMap, null);
 			util.deleteAndWait(NAMESPACE, deployment, service, null);
 		}
 
 	}
-
-	// Create new configmap to trigger controller to signal app to refresh
-	private void createConfigMap() {
-		V1ConfigMap configMap = new V1ConfigMapBuilder().editOrNewMetadata()
-			.withName("service-wiremock")
-			.addToLabels("spring.cloud.kubernetes.config", "true")
-			.endMetadata()
-			.addToData("foo", "bar")
-			.build();
-		util.createAndWait(NAMESPACE, configMap, null);
-	}
-
-	private void deleteConfigMap() {
-		V1ConfigMap configMap = new V1ConfigMapBuilder().editOrNewMetadata()
-			.withName("service-wiremock")
-			.addToLabels("spring.cloud.kubernetes.config", "true")
-			.endMetadata()
-			.addToData("foo", "bar")
-			.build();
-		util.deleteAndWait(NAMESPACE, configMap, null);
-	}
-
-//	private String logs() {
-//		try {
-//			String appPodName = K3S
-//				.execInContainer("sh", "-c",
-//						"kubectl get pods -l app=" + SPRING_CLOUD_K8S_CONFIG_WATCHER_APP_NAME
-//								+ " -o=name --no-headers | tr -d '\n'")
-//				.getStdout();
-//
-//			Container.ExecResult execResult = K3S.execInContainer("sh", "-c", "kubectl logs " + appPodName.trim());
-//			return execResult.getStdout();
-//		}
-//		catch (Exception e) {
-//			e.printStackTrace();
-//			throw new RuntimeException(e);
-//		}
-//	}
 
 }
