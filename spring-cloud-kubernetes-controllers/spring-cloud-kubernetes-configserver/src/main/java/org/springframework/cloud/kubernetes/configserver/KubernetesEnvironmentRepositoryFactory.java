72ea4cea434d5d896abde76dded8586fbc4deb54
Arjav <arjavdongaonkar@gmail.com>
2025-02-10 18:06:03 +0530

    trying existing bean
    Signed-off-by: Arjav <arjavdongaonkar@gmail.com>

--- spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-configserver/src/main/java/org/springframework/cloud/kubernetes/configserver/KubernetesEnvironmentRepositoryFactory.java
+++ spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-configserver/src/main/java/org/springframework/cloud/kubernetes/configserver/KubernetesEnvironmentRepositoryFactory.java
@@ -1,31 +1,20 @@
 package org.springframework.cloud.kubernetes.configserver;
 
-import io.kubernetes.client.openapi.apis.CoreV1Api;
 import org.springframework.cloud.config.server.environment.EnvironmentRepositoryFactory;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class KubernetesEnvironmentRepositoryFactory
 	implements EnvironmentRepositoryFactory<KubernetesEnvironmentRepository, KubernetesConfigServerProperties> {
 
-	private final CoreV1Api coreV1Api;
-	private final List<KubernetesPropertySourceSupplier> kubernetesPropertySourceSuppliers;
+	private final KubernetesEnvironmentRepository kubernetesEnvironmentRepository;
 
-	public KubernetesEnvironmentRepositoryFactory(CoreV1Api coreV1Api,
-												  List<KubernetesPropertySourceSupplier> kubernetesPropertySourceSuppliers) {
-		this.coreV1Api = coreV1Api;
-		this.kubernetesPropertySourceSuppliers = kubernetesPropertySourceSuppliers;
+	public KubernetesEnvironmentRepositoryFactory(KubernetesEnvironmentRepository kubernetesEnvironmentRepository) {
+		this.kubernetesEnvironmentRepository = kubernetesEnvironmentRepository;
 	}
 
 	@Override
 	public KubernetesEnvironmentRepository build(KubernetesConfigServerProperties environmentProperties) {
-		String combinedNamespaces = Stream.of(environmentProperties.getSecretsNamespaces(), environmentProperties.getConfigMapNamespaces())
-			.filter(ns -> ns != null && !ns.isEmpty())
-			.collect(Collectors.joining(","));
-		return new KubernetesEnvironmentRepository(coreV1Api, kubernetesPropertySourceSuppliers, combinedNamespaces);
+		return kubernetesEnvironmentRepository;
 	}
 }
