9b2d60086e613b3b86bf9a2b15f5a9935880e464
wind57 <eugen.rabii@gmail.com>
2026-01-16 22:50:02 +0200

    minor simplification
    Signed-off-by: wind57 <eugen.rabii@gmail.com>

--- spring-cloud-kubernetes-client-discovery/src/main/java/org/springframework/cloud/kubernetes/client/discovery/KubernetesClientInformerAutoConfiguration.java
+++ spring-cloud-kubernetes-client-discovery/src/main/java/org/springframework/cloud/kubernetes/client/discovery/KubernetesClientInformerAutoConfiguration.java
@@ -110,11 +110,12 @@ public final class KubernetesClientInformerAutoConfiguration {
 			List<SharedInformerFactory> sharedInformerFactories, List<String> selectiveNamespaces,
 			ApiClient apiClient) {
 
+		GenericKubernetesApi<V1Service, V1ServiceList> servicesApi = new GenericKubernetesApi<>(V1Service.class,
+			V1ServiceList.class, "", "v1", "services", apiClient);
+
 		int howManyNamespaces = selectiveNamespaces.size();
 		List<SharedIndexInformer<V1Service>> serviceSharedIndexedInformers = new ArrayList<>(howManyNamespaces);
 		for (int i = 0; i < howManyNamespaces; ++i) {
-			GenericKubernetesApi<V1Service, V1ServiceList> servicesApi = new GenericKubernetesApi<>(V1Service.class,
-					V1ServiceList.class, "", "v1", "services", apiClient);
 			SharedIndexInformer<V1Service> sharedIndexInformer = sharedInformerFactories.get(i)
 				.sharedIndexInformerFor(servicesApi, V1Service.class, 0L, selectiveNamespaces.get(i));
 			serviceSharedIndexedInformers.add(sharedIndexInformer);
@@ -147,11 +148,12 @@ public final class KubernetesClientInformerAutoConfiguration {
 			List<SharedInformerFactory> sharedInformerFactories, List<String> selectiveNamespaces,
 			ApiClient apiClient) {
 
+		GenericKubernetesApi<V1Endpoints, V1EndpointsList> endpointsApi = new GenericKubernetesApi<>(
+			V1Endpoints.class, V1EndpointsList.class, "", "v1", "endpoints", apiClient);
+
 		int howManyNamespaces = selectiveNamespaces.size();
 		List<SharedIndexInformer<V1Endpoints>> endpointsSharedIndexedInformers = new ArrayList<>(howManyNamespaces);
 		for (int i = 0; i < howManyNamespaces; ++i) {
-			GenericKubernetesApi<V1Endpoints, V1EndpointsList> endpointsApi = new GenericKubernetesApi<>(
-					V1Endpoints.class, V1EndpointsList.class, "", "v1", "endpoints", apiClient);
 			SharedIndexInformer<V1Endpoints> sharedIndexInformer = sharedInformerFactories.get(i)
 				.sharedIndexInformerFor(endpointsApi, V1Endpoints.class, 0L, selectiveNamespaces.get(i));
 			endpointsSharedIndexedInformers.add(sharedIndexInformer);
@@ -169,7 +171,7 @@ public final class KubernetesClientInformerAutoConfiguration {
 
 		for (int i = 0; i < howManyNamespaces; ++i) {
 			String namespace = selectiveNamespaces.get(i);
-			Lister<V1Endpoints> lister = new Lister<>(endpointsSharedIndexInformers.get(i).getIndexer());
+			Lister<V1Endpoints> lister = new Lister<>(endpointsSharedIndexInformers.get(i).getIndexer(), namespace);
 			LOG.debug(() -> "registering lister (for endpoints) in namespace : " + namespace);
 			endpointsListers.add(lister);
 		}
