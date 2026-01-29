6b9848f0bce30d27852fe3df6af44614a8118023
Ryan Baxter <ryan.baxter@broadcom.com>
2026-01-29 14:24:37 -0500

    Revert skipping tests

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -6,8 +6,8 @@ if [ -f "~/.testcontainers.properties" ]; then
 fi
 echo 'testcontainers.reuse.enable=true' > ~/.testcontainers.properties
 
-#./mvnw clean install -B -Pdocs ${@}
-./mvnw clean install -DskipITs -DskipTests -Dspring-boot.build-image.skip=true -B -Pdocs ${@}
+./mvnw clean install -B -Pdocs ${@}
+#./mvnw clean install -DskipITs -DskipTests -Dspring-boot.build-image.skip=true -B -Pdocs ${@}
 
 rm ~/.testcontainers.properties
 containers=$(docker ps -q)
