a2b14c99ffbf3a56d489623e14d47e1d4759881d
wind57 <eugen.rabii@gmail.com>
2026-03-25 18:11:27 +0200

    simplify code a little
    Signed-off-by: wind57 <eugen.rabii@gmail.com>

--- spring-cloud-kubernetes-fabric8-discovery/src/test/java/org/springframework/cloud/kubernetes/fabric8/discovery/Fabric8DiscoveryClientConfigClientBootstrapConfigurationTests.java
+++ spring-cloud-kubernetes-fabric8-discovery/src/test/java/org/springframework/cloud/kubernetes/fabric8/discovery/Fabric8DiscoveryClientConfigClientBootstrapConfigurationTests.java
@@ -16,60 +16,52 @@
 
 package org.springframework.cloud.kubernetes.fabric8.discovery;
 
-import java.util.Collections;
+import java.util.List;
 import java.util.function.Predicate;
 
 import io.fabric8.kubernetes.api.model.Service;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
-import org.springframework.boot.test.util.TestPropertyValues;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.cloud.client.DefaultServiceInstance;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.commons.util.UtilAutoConfiguration;
 import org.springframework.cloud.config.client.ConfigClientProperties;
 import org.springframework.cloud.config.client.DiscoveryClientConfigServiceBootstrapConfiguration;
-import org.springframework.cloud.kubernetes.commons.KubernetesCommonsAutoConfiguration;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ResolvableType;
 
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.atLeast;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Zhanwei Wang
+ * @author wind57
  */
 class Fabric8DiscoveryClientConfigClientBootstrapConfigurationTests {
 
-	private AnnotationConfigApplicationContext context;
-
-	@AfterEach
-	void afterEach() {
-		if (context != null) {
-			if (context.getParent() != null) {
-				((AnnotationConfigApplicationContext) context.getParent()).close();
-			}
-			context.close();
-		}
-	}
-
 	@Test
 	void onWhenRequested() {
-		setup("server.port=7000", "spring.cloud.config.discovery.enabled=true",
-				"spring.cloud.kubernetes.discovery.enabled:true", "spring.application.name:test",
-				"spring.cloud.config.discovery.service-id:configserver");
-		Assertions.assertThat(context.getParent().getBeanNamesForType(DiscoveryClient.class).length).isEqualTo(1);
-		DiscoveryClient client = context.getParent().getBean(DiscoveryClient.class);
-		verify(client, atLeast(2)).getInstances("configserver");
-		ConfigClientProperties locator = context.getBean(ConfigClientProperties.class);
-		Assertions.assertThat(locator.getUri()[0]).isEqualTo("http://fake:8888/");
+
+		new ApplicationContextRunner()
+			.withUserConfiguration(TestConfig.class,
+				DiscoveryClientConfigServiceBootstrapConfiguration.class, ConfigClientProperties.class)
+			.withPropertyValues(
+				"spring.cloud.config.discovery.enabled=true",
+				"spring.main.cloud-platform=KUBERNETES",
+				"spring.cloud.kubernetes.discovery.enabled=true",
+				"spring.cloud.config.discovery.service-id=configserver")
+			.run(context -> {
+				DiscoveryClient discoveryClient = context.getBean(DiscoveryClient.class);
+				ConfigClientProperties locator = context.getBean(ConfigClientProperties.class);
+				verify(discoveryClient, Mockito.times(1)).getInstances("configserver");
+				assertThat(locator.getUri()[0]).isEqualTo("http://fake:8888/");
+			});
+
 	}
 
 	/**
@@ -80,46 +72,27 @@ class Fabric8DiscoveryClientConfigClientBootstrapConfigurationTests {
 	 */
 	@Test
 	void spelPredicateBeanAvailableInBootstrapContext() {
-		AnnotationConfigApplicationContext parent = new AnnotationConfigApplicationContext();
-		TestPropertyValues
-			.of("spring.cloud.config.discovery.enabled=true", "spring.main.cloud-platform=KUBERNETES",
-					"spring.cloud.kubernetes.discovery.enabled=true")
-			.applyTo(parent);
-		parent.register(PropertyPlaceholderAutoConfiguration.class, KubernetesCommonsAutoConfiguration.class,
-				Fabric8DiscoveryClientConfigClientBootstrapConfiguration.class);
-		parent.refresh();
 
-		context = new AnnotationConfigApplicationContext();
-		context.setParent(parent);
-		context.refresh();
-
-		ResolvableType predicateType = ResolvableType.forClassWithGenerics(Predicate.class, Service.class);
-		Assertions.assertThat(parent.getBeanNamesForType(predicateType)).isNotEmpty();
+		new ApplicationContextRunner()
+			.withUserConfiguration(Fabric8DiscoveryClientConfigClientBootstrapConfiguration.class)
+			.withPropertyValues(
+				"spring.cloud.config.discovery.enabled=true",
+				"spring.main.cloud-platform=KUBERNETES",
+				"spring.cloud.kubernetes.discovery.enabled=true")
+			.run(context -> {
+				ResolvableType servicesPredicate = ResolvableType.forClassWithGenerics(Predicate.class, Service.class);
+				assertThat(context.getBeanNamesForType(servicesPredicate)).hasSize(1);
+			});
 	}
 
-	private void setup(String... env) {
-		AnnotationConfigApplicationContext parent = new AnnotationConfigApplicationContext();
-		TestPropertyValues.of(env).applyTo(parent);
-		parent.register(UtilAutoConfiguration.class, PropertyPlaceholderAutoConfiguration.class,
-				EnvironmentKnobbler.class, KubernetesCommonsAutoConfiguration.class,
-				Fabric8DiscoveryClientConfigClientBootstrapConfiguration.class,
-				DiscoveryClientConfigServiceBootstrapConfiguration.class, ConfigClientProperties.class);
-		parent.refresh();
-		context = new AnnotationConfigApplicationContext();
-		context.setParent(parent);
-		context.register(PropertyPlaceholderAutoConfiguration.class, KubernetesCommonsAutoConfiguration.class,
-				Fabric8DiscoveryClientAutoConfiguration.class);
-		context.refresh();
-	}
-
-	@Configuration(proxyBeanMethods = false)
-	protected static class EnvironmentKnobbler {
+	@TestConfiguration
+	static class TestConfig {
 
 		@Bean
 		Fabric8DiscoveryClient kubernetesDiscoveryClient() {
 			Fabric8DiscoveryClient client = mock(Fabric8DiscoveryClient.class);
 			ServiceInstance instance = new DefaultServiceInstance("configserver1", "configserver", "fake", 8888, false);
-			given(client.getInstances("configserver")).willReturn(Collections.singletonList(instance));
+			when(client.getInstances("configserver")).thenReturn(List.of(instance));
 			return client;
 		}
 
