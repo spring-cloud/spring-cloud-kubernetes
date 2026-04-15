4d8c19de85979ccd774116cbd12047b3e8a1e744
wind57 <eugen.rabii@gmail.com>
2026-04-15 12:32:51 +0300

    first comment from copilot
    Signed-off-by: wind57 <eugen.rabii@gmail.com>

--- spring-cloud-kubernetes-client-config/src/main/java/org/springframework/cloud/kubernetes/client/config/reload/KubernetesClientEventBasedConfigMapChangeDetector.java
+++ spring-cloud-kubernetes-client-config/src/main/java/org/springframework/cloud/kubernetes/client/config/reload/KubernetesClientEventBasedConfigMapChangeDetector.java
@@ -123,7 +123,6 @@ public class KubernetesClientEventBasedConfigMapChangeDetector extends Configura
 
 	@PostConstruct
 	void inform() {
-		LOG.info(() -> "Kubernetes event-based configMap change detector activated");
 		if (monitoringConfigMaps) {
 			LOG.info(() -> "Kubernetes event-based configMap change detector activated");
 
