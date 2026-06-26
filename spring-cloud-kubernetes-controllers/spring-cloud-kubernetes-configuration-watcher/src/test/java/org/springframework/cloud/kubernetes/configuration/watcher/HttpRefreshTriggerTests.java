de5d86049de4f0cca66e23576d547ba16df51f96
wind57 <eugen.rabii@gmail.com>
2026-06-26 17:27:40 +0300

    add more tests
    Signed-off-by: wind57 <eugen.rabii@gmail.com>

--- spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-configuration-watcher/src/test/java/org/springframework/cloud/kubernetes/configuration/watcher/HttpRefreshTriggerTests.java
+++ spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-configuration-watcher/src/test/java/org/springframework/cloud/kubernetes/configuration/watcher/HttpRefreshTriggerTests.java
@@ -16,6 +16,7 @@
 
 package org.springframework.cloud.kubernetes.configuration.watcher;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -36,6 +37,8 @@ import org.springframework.cloud.kubernetes.commons.discovery.DefaultKubernetesS
 import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -78,11 +81,15 @@ class HttpRefreshTriggerTests {
 
 		when(reactiveDiscoveryClient.getServices()).thenReturn(Flux.just("app-one", "app-two"));
 		when(reactiveDiscoveryClient.getInstances(eq("app-one")))
-			.thenReturn(Flux.just(serviceInstance("app-one", Map.of("app", "demo", "tier", "backend"))));
+			.thenReturn(Flux.just(serviceInstance("app-one", "/app-one/actuator",
+					Map.of("app", "demo", "tier", "backend"))));
 		when(reactiveDiscoveryClient.getInstances(eq("app-two")))
-			.thenReturn(Flux.just(serviceInstance("app-two", Map.of("app", "demo", "tier", "frontend"))));
+			.thenReturn(Flux.just(serviceInstance("app-two", "/app-two/actuator",
+					Map.of("app", "demo", "tier", "frontend"))));
 
-		WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/actuator/refresh"))
+		WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/app-one/actuator/refresh"))
+			.willReturn(WireMock.aResponse().withStatus(200)));
+		WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/app-two/actuator/refresh"))
 			.willReturn(WireMock.aResponse().withStatus(200)));
 
 		Map<String, String> inputLabels = Map.of("app", "demo", "tier", "backend");
@@ -91,12 +98,76 @@ class HttpRefreshTriggerTests {
 		// subscribe and assert that it did not fail
 		StepVerifier.create(refreshTrigger.triggerRefresh(source)).verifyComplete();
 
-		WireMock.verify(1, WireMock.postRequestedFor(WireMock.urlEqualTo("/actuator/refresh")));
+		WireMock.verify(1, WireMock.postRequestedFor(WireMock.urlEqualTo("/app-one/actuator/refresh")));
+		WireMock.verify(0, WireMock.postRequestedFor(WireMock.urlEqualTo("/app-two/actuator/refresh")));
 	}
 
-	private DefaultKubernetesServiceInstance serviceInstance(String serviceId, Map<String, String> metadata) {
-		return new DefaultKubernetesServiceInstance(serviceId, serviceId, "localhost", wireMockServer.port(), metadata,
-				false, "default", null, Map.of());
+	/**
+	 * <pre>
+	 *     - input labels are empty
+	 *     - we search directly by service name
+	 *     - discovery getServices is not used
+	 * </pre>
+	 */
+	@Test
+	void refreshesServiceInstancesByNameWhenLabelsAreMissing() {
+		ConfigurationWatcherConfigurationProperties properties = new ConfigurationWatcherConfigurationProperties();
+		HttpRefreshTrigger refreshTrigger = new HttpRefreshTrigger(reactiveDiscoveryClient, properties,
+				WebClient.builder().build());
+
+		when(reactiveDiscoveryClient.getInstances(eq("app-one")))
+			.thenReturn(Flux.just(serviceInstance("app-one", "/app-one/actuator", Map.of())));
+
+		WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/app-one/actuator/refresh"))
+			.willReturn(WireMock.aResponse().withStatus(200)));
+
+		KubernetesSource source = new ConfigMapKubernetesSource(Set.of("app-one"), Map.of(), "my-configmap");
+
+		StepVerifier.create(refreshTrigger.triggerRefresh(source)).verifyComplete();
+
+		WireMock.verify(1, WireMock.postRequestedFor(WireMock.urlEqualTo("/app-one/actuator/refresh")));
+		verify(reactiveDiscoveryClient, never()).getServices();
+	}
+
+	/**
+	 * <pre>
+	 *     - discovered service instance labels do not match input labels
+	 *     - no refresh request is sent
+	 * </pre>
+	 */
+	@Test
+	void doesNotRefreshWhenNoServiceInstanceMatchesLabels() {
+		ConfigurationWatcherConfigurationProperties properties = new ConfigurationWatcherConfigurationProperties();
+		HttpRefreshTrigger refreshTrigger = new HttpRefreshTrigger(reactiveDiscoveryClient, properties,
+				WebClient.builder().build());
+
+		when(reactiveDiscoveryClient.getServices()).thenReturn(Flux.just("app-one"));
+		when(reactiveDiscoveryClient.getInstances(eq("app-one")))
+			.thenReturn(Flux.just(serviceInstance("app-one", "/app-one/actuator",
+					Map.of("app", "demo", "tier", "frontend"))));
+
+		WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/app-one/actuator/refresh"))
+			.willReturn(WireMock.aResponse().withStatus(200)));
+
+		KubernetesSource source = new ConfigMapKubernetesSource(Set.of(), Map.of("app", "demo", "tier", "backend"),
+				"my-configmap");
+
+		StepVerifier.create(refreshTrigger.triggerRefresh(source)).verifyComplete();
+
+		WireMock.verify(0, WireMock.postRequestedFor(WireMock.urlEqualTo("/app-one/actuator/refresh")));
+	}
+
+	private DefaultKubernetesServiceInstance serviceInstance(String serviceId, String actuatorPath,
+			Map<String, String> metadata) {
+		return new DefaultKubernetesServiceInstance(serviceId, serviceId, "localhost", wireMockServer.port(),
+				metadataWithActuatorPath(metadata, actuatorPath), false, "default", null, Map.of());
+	}
+
+	private Map<String, String> metadataWithActuatorPath(Map<String, String> metadata, String actuatorPath) {
+		Map<String, String> metadataWithActuatorPath = new HashMap<>(metadata);
+		metadataWithActuatorPath.put(ConfigurationWatcherConfigurationProperties.ANNOTATION_KEY,
+				"http://:" + wireMockServer.port() + actuatorPath);
+		return metadataWithActuatorPath;
 	}
 
 }
