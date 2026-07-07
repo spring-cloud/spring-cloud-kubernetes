a6cc9bae51be334001c488d9b0057abbc5b74efb
wind57 <eugen.rabii@gmail.com>
2026-07-07 09:42:30 +0300

    refactor to records
    Signed-off-by: wind57 <eugen.rabii@gmail.com>

--- spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-configuration-watcher/src/main/java/org/springframework/cloud/kubernetes/configuration/watcher/ConfigMapKubernetesSource.java
+++ spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-configuration-watcher/src/main/java/org/springframework/cloud/kubernetes/configuration/watcher/ConfigMapKubernetesSource.java
@@ -22,28 +22,12 @@ import java.util.Set;
 /**
  * @author wind57
  */
-final class ConfigMapKubernetesSource implements KubernetesSource {
+record ConfigMapKubernetesSource(Set<String> serviceNames, Map<String, String> serviceLabels,
+		String resourceName) implements KubernetesSource {
 
 	static final String CONFIGMAP_SERVICE_NAMES_ANNOTATION = "spring.cloud.kubernetes.configmap.apps";
 
 	static final String CONFIGMAP_SERVICE_LABELS_ANNOTATION = "spring.cloud.kubernetes.configmap.labels";
-
-	private final Set<String> serviceNames;
-
-	private final Map<String, String> serviceLabels;
-
-	private final String resourceName;
-
-	ConfigMapKubernetesSource(Set<String> serviceNames, Map<String, String> serviceLabels, String resourceName) {
-		this.serviceNames = serviceNames;
-		this.serviceLabels = serviceLabels;
-		this.resourceName = resourceName;
-	}
-
-	@Override
-	public String resourceName() {
-		return resourceName;
-	}
 
 	@Override
 	public String description() {
@@ -53,16 +37,6 @@ final class ConfigMapKubernetesSource implements KubernetesSource {
 	@Override
 	public String requiredResourceLabel() {
 		return ConfigurationWatcherConfigurationProperties.CONFIG_MAP_LABEL;
-	}
-
-	@Override
-	public Set<String> serviceNames() {
-		return serviceNames;
-	}
-
-	@Override
-	public Map<String, String> serviceLabels() {
-		return serviceLabels;
 	}
 
 	@Override
