3136b95093f9d81320eabe12f5264967e00e37ac
wind57 <eugen.rabii@gmail.com>
2026-09-20 23:37:31 +0300

    fix test
    Signed-off-by: wind57 <eugen.rabii@gmail.com>

--- spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-k8s-client-reload/src/test/java/org/springframework/cloud/kubernetes/k8s/client/reload/it/K8sClientConfigMapLabelEventTriggeredIT.java
+++ spring-cloud-kubernetes-integration-tests/spring-cloud-kubernetes-k8s-client-reload/src/test/java/org/springframework/cloud/kubernetes/k8s/client/reload/it/K8sClientConfigMapLabelEventTriggeredIT.java
@@ -104,7 +104,7 @@ class K8sClientConfigMapLabelEventTriggeredIT extends K8sClientReloadBase {
 	void test(CapturedOutput output) {
 
 		assertReloadLogStatements(
-				"add configmap informer for namespace : "
+				"added configmap informer for namespace : "
 						+ "right with labels : {spring.cloud.kubernetes.config.informer.enabled=true}",
 				"added secret informer for namespace", output);
 
